14be6a4be887355c272a836f4930c7208e42a51b
Hong Yao <ganhongyao@u.nus.edu>
2022-09-30 01:31:07 +0800

    Add primary donor model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_171009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "primary_donors", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_primary_donors_on_email", unique: true
+  end
 
 end
