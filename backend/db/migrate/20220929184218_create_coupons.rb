73a85c99c5267afa8098f512467a373db735bd09
Hong Yao <ganhongyao@u.nus.edu>
2022-09-30 02:46:19 +0800

    Add coupon model

---
+++ backend/db/migrate/20220929184218_create_coupons.rb
@@ -0,0 +1,13 @@
+class CreateCoupons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :coupons do |t|
+      t.string :url_token, null: false
+      t.integer :denomination, null: false
+      t.boolean :is_redeemed, null: false, default: false
+      t.references :campaign, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :coupons, :url_token, unique: true
+  end
+end
