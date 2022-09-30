bf63f5206bdb24575654c8eb3c145886114d7216
Hong Yao <ganhongyao@u.nus.edu>
2022-09-30 15:51:48 +0800

    Add % 10 validation for campaign promised amount

--- backend/app/models/campaign.rb
+++ backend/app/models/campaign.rb
@@ -13,4 +13,13 @@ class Campaign < ApplicationRecord
   validates :promised_amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :start, presence: true
   validates :end, comparison: { greater_than: :start }
+  validate :promised_amount_multiple_of_10
+
+  private
+
+  def promised_amount_multiple_of_10
+    return if promised_amount % 10 == 0
+
+    errors.add(:promised_amount, 'must be a multiple of 10')
+  end
 end
