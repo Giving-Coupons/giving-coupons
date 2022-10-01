097f49f5b7fb8435afb1b68b534942273edbd17f
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-10-01 17:46:09 +0800

    Update routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   scope :api do
     scope :v1 do
-      mount_devise_token_auth_for 'Admin', at: 'auth', controllers: {
+      mount_devise_token_auth_for 'Admin', at: 'auth', skip: %i[passwords token_validations], controllers: {
         registrations: 'auth/registrations',
         sessions: 'auth/sessions'
       }
