4aabe1984ab7fed1d35e165ced20bb502a750466
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-10-01 17:56:01 +0800

    Fix sign_out not working

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -25,5 +25,10 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Authentication
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
