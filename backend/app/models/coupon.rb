af72cfe807a4e903d296aee28b579d4dc60fe0c4
Hong Yao <ganhongyao@u.nus.edu>
2022-10-03 02:22:01 +0800

    Make is_redeemed a derived property on coupons

--- backend/app/models/coupon.rb
+++ backend/app/models/coupon.rb
@@ -9,6 +9,10 @@ class Coupon < ApplicationRecord
   validates :url_token, presence: true, uniqueness: true
   validates :denomination, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
+  def is_redeemed?
+    secondary_donation.present?
+  end
+
   def self.generate_unique_url_token
     token = SecureRandom.alphanumeric(NUM_ALPHANUMERIC_CHARS_IN_TOKEN)
     Coupon.exists?(url_token: token) ? generate_unique_url_token : token
