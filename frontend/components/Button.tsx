b88e1d05cc09455afe9531c5194fd92775ef602a
Geng Ning <gengninghan1@gmail.com>
2022-10-06 19:04:07 +0800

    Add basic theme

---
+++ frontend/components/Button.tsx
@@ -0,0 +1,30 @@
+import { Button as MuiButton, ButtonProps as MuiButtonProps } from '@mui/material';
+
+interface Props extends MuiButtonProps {
+  actionType: 'primary' | 'secondary' | 'tertiary';
+}
+
+const Button = ({ actionType, children, ...rest }: Props) => {
+  switch (actionType) {
+    case 'primary':
+      return (
+        <MuiButton color="primary" variant="contained" {...rest}>
+          {children}
+        </MuiButton>
+      );
+    case 'secondary':
+      return (
+        <MuiButton color="primary" variant="outlined" {...rest}>
+          {children}
+        </MuiButton>
+      );
+    case 'tertiary':
+      return (
+        <MuiButton color="primary" variant="text" {...rest}>
+          {children}
+        </MuiButton>
+      );
+  }
+};
+
+export default Button;
---
+++ frontend/utils/theme.ts
@@ -0,0 +1,66 @@
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    contrast: Palette['primary'];
+  }
+  interface PaletteOptions {
+    contrast: PaletteOptions['primary'];
+  }
+}
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#FFE4AE',
+      main: '#FF7200',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      light: '#FFE7AB',
+      main: '#FFC428',
+    },
+    contrast: {
+      light: '#BDE7FF',
+      main: '#1E76DD',
+      dark: '#002584',
+    },
+    error: {
+      main: '#FF4343',
+    },
+    warning: {
+      main: '#FF9315',
+    },
+    info: {
+      main: '#1E76DD',
+    },
+    success: {
+      main: '#09B4A9',
+    },
+  },
+  components: {
+    MuiButton: {
+      variants: [
+        {
+          props: { variant: 'contained', color: 'primary' },
+          style: {
+            borderRadius: 30,
+            boxShadow: '0px 0px 8px 0px #FF9315',
+          },
+        },
+        {
+          props: { variant: 'outlined', color: 'primary' },
+          style: {
+            borderRadius: 30,
+          },
+        },
+        {
+          props: { variant: 'text', color: 'primary' },
+          style: {
+            borderRadius: 30,
+          },
+        },
+      ],
+    },
+  },
+});
