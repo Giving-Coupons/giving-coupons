ae74f50d5ba7c186953f9eafa04451328408514b
Geng Ning <gengninghan1@gmail.com>
2022-10-08 23:55:47 +0800

    Clean up campaign list card

--- frontend/components/CampaignListCard.tsx
+++ frontend/components/CampaignListCard.tsx
@@ -1,37 +1,16 @@
-import { Box, Container, Stack, SxProps } from '@mui/system';
+import { Box, Container, Stack } from '@mui/system';
 import { Typography, Grid } from '@mui/material';
 import Button from './Button';
 import CompetingGraph from './CompetingGraph';
 import { CampaignListData } from '../types/campaigns';
 import CardWithImage from './CardWithImage';
-
-const graphContainerSx: SxProps = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'end',
-  height: '100%',
-  paddingBottom: '8px',
-};
-
-const graphSx: SxProps = {
-  height: '1em',
-  width: '100%',
-};
-
-const descriptionContainerSx: SxProps = {
-  width: '100%',
-  overflow: 'auto',
-};
-
-const beneficiaryLogoSx: SxProps = {
-  maxHeight: '48px',
-  minHeight: '48px',
-  height: '48px',
-};
-
-const buttonSx: SxProps = {
-  width: '100%',
-};
+import {
+  charityLogoSx,
+  descriptionContainerSx,
+  graphContainerSx,
+  graphSx,
+  buttonSx,
+} from '../styles/components/CapaignListCardStyles';
 
 interface Props {
   campaign: CampaignListData;
@@ -55,41 +34,41 @@ const CampaignListCard = ({ campaign }: Props) => {
     </Container>
   );
 
-  const descriptionContent = (
-    <>
-      <Stack sx={descriptionContainerSx} spacing={0.5} component="div">
-        <Typography variant="h3">Campaign Name</Typography>
+  const description = (
+    <Stack sx={descriptionContainerSx} spacing={0.5} component="div">
+      <Typography variant="h3">Campaign Name</Typography>
 
-        <Typography variant="caption">For the following beneficiaries</Typography>
+      <Typography variant="caption">For the following beneficiaries</Typography>
 
-        <Grid container>
-          {campaign.charities.map((charity, index) => (
-            <Grid item xs={3} key={index}>
-              <Box sx={beneficiaryLogoSx} component="img" src={charity.logoUrl} />
-            </Grid>
-          ))}
-        </Grid>
+      <Grid container>
+        {campaign.charities.map((charity, index) => (
+          <Grid item xs={3} key={index}>
+            <Box sx={charityLogoSx} component="img" src={charity.logoUrl} />
+          </Grid>
+        ))}
+      </Grid>
 
-        <Typography variant="caption">{campaign.description}</Typography>
-      </Stack>
+      <Typography variant="caption">{campaign.description}</Typography>
+    </Stack>
+  );
 
-      <Box>
-        <Button sx={buttonSx} actionType="primary">
-          Contribute
-        </Button>
+  const actionButtons = (
+    <Box>
+      <Button sx={buttonSx} actionType="primary">
+        Contribute
+      </Button>
 
-        <Button sx={buttonSx} actionType="tertiary">
-          Learn more
-        </Button>
-      </Box>
-    </>
+      <Button sx={buttonSx} actionType="tertiary">
+        Learn more
+      </Button>
+    </Box>
   );
 
   return (
     <CardWithImage
       imageUrl={campaign.imageUrl}
       imageOverlayContent={imageOverlayContent}
-      descriptionContent={descriptionContent}
+      descriptionContent={[description, actionButtons]}
     />
   );
 };
