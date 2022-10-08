3f2493574fcfcd575c9dab2d9a1a0574a5f461c9
Geng Ning <gengninghan1@gmail.com>
2022-10-08 23:45:42 +0800

    Extract card with image

--- frontend/components/CampaignListCard.tsx
+++ frontend/components/CampaignListCard.tsx
@@ -1,29 +1,9 @@
 import { Box, Container, Stack, SxProps } from '@mui/system';
-import { theme } from '../utils/theme';
 import { Typography, Grid } from '@mui/material';
 import Button from './Button';
-import ImageWithOverlay from './ImageWithOverlay';
 import CompetingGraph from './CompetingGraph';
 import { CampaignListData } from '../types/campaigns';
-
-const borderRadius = '20px';
-
-const containerSx: SxProps = {
-  alignItems: 'center',
-  height: '100%',
-  boxShadow: `0px 0px 8px 0px ${theme.palette.neutral.light}`,
-  borderRadius: borderRadius,
-};
-
-const topContainerSx: SxProps = {
-  minWidth: '100%',
-  maxWidth: '100%',
-  minHeight: '40%',
-  height: '40%',
-  maxHeight: '40%',
-  borderTopLeftRadius: borderRadius,
-  borderTopRightRadius: borderRadius,
-};
+import CardWithImage from './CardWithImage';
 
 const graphContainerSx: SxProps = {
   display: 'flex',
@@ -36,15 +16,6 @@ const graphContainerSx: SxProps = {
 const graphSx: SxProps = {
   height: '1em',
   width: '100%',
-};
-
-const bottomContainerSx: SxProps = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-  margin: '4px 0',
-  width: '90%',
-  height: '100%',
 };
 
 const descriptionContainerSx: SxProps = {
@@ -73,49 +44,53 @@ const CampaignListCard = ({ campaign }: Props) => {
   const bottomLabels = [`$${secondaryDonorDonationData.amount}`, 'by the secondary donor'];
   const barFractions = [primaryDonorDonationData.fraction, secondaryDonorDonationData.fraction];
 
+  const imageOverlayContent = (
+    <Container sx={graphContainerSx} component="div">
+      <CompetingGraph
+        overrideGraphSx={graphSx}
+        topLabels={topLabels}
+        bottomLabels={bottomLabels}
+        barFractions={barFractions}
+      />
+    </Container>
+  );
+
+  const descriptionContent = (
+    <>
+      <Stack sx={descriptionContainerSx} spacing={0.5} component="div">
+        <Typography variant="h3">Campaign Name</Typography>
+
+        <Typography variant="caption">For the following beneficiaries</Typography>
+
+        <Grid container>
+          {campaign.charities.map((charity, index) => (
+            <Grid item xs={3} key={index}>
+              <Box sx={beneficiaryLogoSx} component="img" src={charity.logoUrl} />
+            </Grid>
+          ))}
+        </Grid>
+
+        <Typography variant="caption">{campaign.description}</Typography>
+      </Stack>
+
+      <Box>
+        <Button sx={buttonSx} actionType="primary">
+          Contribute
+        </Button>
+
+        <Button sx={buttonSx} actionType="tertiary">
+          Learn more
+        </Button>
+      </Box>
+    </>
+  );
+
   return (
-    <Stack component="div" sx={containerSx}>
-      <Box sx={topContainerSx}>
-        <ImageWithOverlay imageSrc={campaign.imageUrl}>
-          <Container sx={graphContainerSx} component="div">
-            <CompetingGraph
-              overrideGraphSx={graphSx}
-              topLabels={topLabels}
-              bottomLabels={bottomLabels}
-              barFractions={barFractions}
-            />
-          </Container>
-        </ImageWithOverlay>
-      </Box>
-
-      <Box sx={bottomContainerSx} component="div">
-        <Stack sx={descriptionContainerSx} spacing={0.5} component="div">
-          <Typography variant="h3">Campaign Name</Typography>
-
-          <Typography variant="caption">For the following beneficiaries</Typography>
-
-          <Grid container>
-            {campaign.charities.map((charity, index) => (
-              <Grid item xs={3} key={index}>
-                <Box sx={beneficiaryLogoSx} component="img" src={charity.logoUrl} />
-              </Grid>
-            ))}
-          </Grid>
-
-          <Typography variant="caption">{campaign.description}</Typography>
-        </Stack>
-
-        <Box>
-          <Button sx={buttonSx} actionType="primary">
-            Contribute
-          </Button>
-
-          <Button sx={buttonSx} actionType="tertiary">
-            Learn more
-          </Button>
-        </Box>
-      </Box>
-    </Stack>
+    <CardWithImage
+      imageUrl={campaign.imageUrl}
+      imageOverlayContent={imageOverlayContent}
+      descriptionContent={descriptionContent}
+    />
   );
 };
 
---
+++ frontend/components/CardWithImage.tsx
@@ -0,0 +1,26 @@
+import { Box, Stack } from '@mui/system';
+import { descriptionContainerSx, containerSx, imageContainerSx } from '../styles/components/CardWithImageStyles';
+import ImageWithOverlay from './ImageWithOverlay';
+import React from 'react';
+
+interface Props {
+  imageUrl: string;
+  imageOverlayContent: React.ReactNode;
+  descriptionContent: React.ReactNode;
+}
+
+const CardWithImage = ({ imageUrl, imageOverlayContent, descriptionContent }: Props) => {
+  return (
+    <Stack component="div" sx={containerSx}>
+      <Box sx={imageContainerSx}>
+        <ImageWithOverlay imageSrc={imageUrl}>{imageOverlayContent}</ImageWithOverlay>
+      </Box>
+
+      <Box sx={descriptionContainerSx} component="div">
+        {descriptionContent}
+      </Box>
+    </Stack>
+  );
+};
+
+export default CardWithImage;
