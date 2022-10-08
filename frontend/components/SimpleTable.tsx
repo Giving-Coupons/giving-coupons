0594c521e8582afb445356e3711bf4db14a6cd04
Hong Yao <ganhongyao@u.nus.edu>
2022-10-09 00:43:27 +0800

    Add message when table has no data

--- frontend/components/SimpleTable.tsx
+++ frontend/components/SimpleTable.tsx
@@ -48,24 +48,30 @@ export default function SimpleTable<D>({ columns, rows, actions = [], isLoading 
               <CircularProgress />
             </TableCell>
           )}
-          {rows.map((row, index) => (
-            <TableRow key={index}>
-              {columns.map(({ key, transformValue = (value) => value }, index) => (
-                <TableCell key={index}>{transformValue(row[key])}</TableCell>
-              ))}
-              {hasActions && (
-                <TableCell>
-                  <Grid container>
-                    {actions?.map((action, index) => (
-                      <Grid item key={index} xs={12} lg={3}>
-                        {React.cloneElement(action.component, { onClick: () => action.onClick(row) })}
-                      </Grid>
-                    ))}
-                  </Grid>
-                </TableCell>
-              )}
-            </TableRow>
-          ))}
+          {rows.length === 0 ? (
+            <TableCell colSpan={numColumns} align="center" sx={{ color: 'lightgray' }}>
+              No data to be displayed.
+            </TableCell>
+          ) : (
+            rows.map((row, index) => (
+              <TableRow key={index}>
+                {columns.map(({ key, transformValue = (value) => value }, index) => (
+                  <TableCell key={index}>{transformValue(row[key])}</TableCell>
+                ))}
+                {hasActions && (
+                  <TableCell>
+                    <Grid container>
+                      {actions?.map((action, index) => (
+                        <Grid item key={index} xs={12} lg={3}>
+                          {React.cloneElement(action.component, { onClick: () => action.onClick(row) })}
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </TableCell>
+                )}
+              </TableRow>
+            ))
+          )}
         </TableBody>
       </Table>
     </TableContainer>
