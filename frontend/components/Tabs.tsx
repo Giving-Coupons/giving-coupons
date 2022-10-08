2911ce9873677809b5fd6af0b1de7ab1f9c7a609
Hong Yao <ganhongyao@u.nus.edu>
2022-10-09 00:54:16 +0800

    Remove commented out code

--- frontend/components/Tabs.tsx
+++ frontend/components/Tabs.tsx
@@ -49,15 +49,6 @@ export default function Tabbed({ tabs }: TabbedProps) {
           {tab.content}
         </TabPanel>
       ))}
-      {/* <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel> */}
     </Box>
   );
 }
