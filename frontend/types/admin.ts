29ea5a48cd0e8535ced2eb4e27b0443b82285595
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-10-09 01:13:41 +0800

    Fix password validation error message. (casing)

--- frontend/types/admin.ts
+++ frontend/types/admin.ts
@@ -14,7 +14,9 @@ export type AdminLoginData = InferType<typeof adminLoginDataSchema>;
 
 export const adminPostDataSchema = object({
   username: string().required('Username is a required field.'),
-  password: string().required().min(6, `The new user's password must be at least 6 characters long.`),
+  password: string()
+    .required('Password is a required field.')
+    .min(6, `The new user's password must be at least 6 characters long.`),
   passwordConfirmation: string()
     .required('Password confirmation is a required field.')
     .oneOf([ref('password'), null], 'Passwords must match'),
