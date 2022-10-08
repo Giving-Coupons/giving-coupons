87213bea8fb8aa9223b657fe365d852103ceccb1
Hong Yao <ganhongyao@u.nus.edu>
2022-10-08 22:10:20 +0800

    Use common endpoint

--- frontend/pages/admin/interests/index.tsx
+++ frontend/pages/admin/interests/index.tsx
@@ -6,7 +6,7 @@ import DeleteButton from '../../../components/DeleteButton';
 import DoneIcon from '@mui/icons-material/Done';
 import ClearIcon from '@mui/icons-material/Clear';
 import Tabbed from '../../../components/Tabs';
-import { Box, CircularProgress, Paper, Typography } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
 import useSWR from 'swr';
 import api from '../../../frontendApis';
 import { Nullable } from '../../../types/utils';
@@ -14,17 +14,13 @@ import { theme } from '../../../utils/theme';
 import InterestsAPI from '../../../frontendApis/interests';
 
 export default function Interests() {
-  const { data: pendingInterests } = useSWR<Nullable<InterestData[]>>(InterestsAPI.INTERESTS_URL, () =>
-    api.interests.getPendingInterests().then((r) => r.payload),
+  const { data: interests } = useSWR<Nullable<InterestData[]>>(InterestsAPI.INTERESTS_URL, () =>
+    api.interests.getInterests().then((r) => r.payload),
   );
 
-  // const { data: approvedInterests } = useSWR<Nullable<InterestData[]>>('/api/interests/approved', () =>
-  //   api.interests.getAcceptedInterests().then((r) => r.payload),
-  // );
-
-  // const { data: rejectedInterests } = useSWR<Nullable<InterestData[]>>('/api/interests/rejected', () =>
-  //   api.interests.getAcceptedInterests().then((r) => r.payload),
-  // );
+  const pendingInterests = interests?.filter((interest) => interest.status === InterestStatus.PENDING);
+  const approvedInterests = interests?.filter((interest) => interest.status === InterestStatus.APPROVED);
+  const rejectedInterests = interests?.filter((interest) => interest.status === InterestStatus.REJECTED);
 
   const makeInterestsTable = (interests: Nullable<InterestData[]> | undefined) => (
     <SimpleTable
@@ -37,7 +33,6 @@ export default function Interests() {
         { title: 'Promised Amount', key: 'promisedAmount' },
         { title: 'Start', key: 'start', transformValue: (value) => new Date(value).toLocaleDateString() },
         { title: 'End', key: 'end', transformValue: (value) => new Date(value).toLocaleDateString() },
-        { title: 'Status', key: 'status' },
         { title: 'Coupon Denomination', key: 'couponDenomination' },
       ]}
       rows={interests ?? []}
@@ -69,11 +64,11 @@ export default function Interests() {
             },
             {
               label: 'Approved',
-              content: makeInterestsTable(pendingInterests),
+              content: makeInterestsTable(approvedInterests),
             },
             {
               label: 'Rejected',
-              content: makeInterestsTable(pendingInterests),
+              content: makeInterestsTable(rejectedInterests),
             },
           ]}
         />
