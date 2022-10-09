947c7992375735ef8b629eb20988d4663cceba0b
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-10-09 15:59:05 +0800

    Update CustomForm to use function style instead

--- frontend/hooks/useCustomForm.tsx
+++ frontend/hooks/useCustomForm.tsx
@@ -6,24 +6,25 @@ import Box from '@mui/material/Box';
 import { useSnackbar } from 'notistack';
 import { Alert, AlertTitle, Button, CssBaseline, InputAdornment, List, ListItemText } from '@mui/material';
 import { LocalizationProvider, MobileDatePicker } from '@mui/x-date-pickers';
-import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
-import { DateTime } from 'luxon';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { ApiPromise } from '../types/api';
+import moment from 'moment';
 
 interface SubmitState {
   canSubmit: boolean;
   messages: string[];
 }
 
-interface Props<T> {
-  schema: Yup.SchemaOf<T>;
-  submitFn: (arg: T) => ApiPromise<T>;
+export interface CustomFormProps<T> {
+  initialData?: Partial<T>;
+  schema: Yup.AnyObjectSchema;
+  submitFn: (arg: T) => ApiPromise<unknown>;
   renderFn: (args: FormRenderFunctionProps<T>) => ReactNode;
 }
 
 type FormDataKeys<T> = Extract<keyof T, string>;
 
-type FormRenderFunctionProps<T> = {
+export type FormRenderFunctionProps<T> = {
   // Default component generators
   createDatePicker: (field: FormDataKeys<T>, label: string) => ReactNode;
   createTextField: (field: FormDataKeys<T>, label: string) => ReactNode;
@@ -32,24 +33,26 @@ type FormRenderFunctionProps<T> = {
   createErrorMessages: () => ReactNode;
 
   // If custom components are required, you may use these
+  handleChange: (field: FormDataKeys<T>, value: any) => void;
   canSubmit: boolean;
   errorMessages: string[];
   currentValue: Partial<T>;
 };
 
-export default function CustomForm<T extends Record<string, string | number | DateTime>>({
+export default function useCustomForm<T extends Record<string, any>>({
+  initialData,
   schema,
   submitFn,
   renderFn,
-}: Props<T>): ReactNode {
+}: CustomFormProps<T>): JSX.Element {
   const { enqueueSnackbar } = useSnackbar();
   const [submitState, setSubmitState] = useState<SubmitState>({ canSubmit: false, messages: [] });
-  const [formData, setFormData] = useState<Partial<T>>({});
+  const [formData, setFormData] = useState<Partial<T>>(initialData ?? {});
 
   const createDatePicker = (field: FormDataKeys<T>, label: string) => (
     <Grid item xs={12}>
       <MobileDatePicker
-        inputFormat="EEE, dd MMM yyyy"
+        inputFormat="ddd, D MMM yyyy"
         value={formData ? formData[field] ?? null : null}
         toolbarTitle={label}
         onChange={handleDateChange(field)}
@@ -111,17 +114,16 @@ export default function CustomForm<T extends Record<string, string | number | Da
     );
   };
 
-  const handleDateChange = (prop: FormDataKeys<T>) => (value: DateTime | null) => {
-    const newState = { ...formData, [prop]: value ?? undefined } as Partial<T>;
-    setFormData(newState);
-    schema
-      .validate(newState, { abortEarly: false })
-      .then(() => setSubmitState({ canSubmit: true, messages: [] }))
-      .catch((err) => setSubmitState({ canSubmit: false, messages: err.errors }));
+  const handleDateChange = (prop: FormDataKeys<T>) => (value: moment.Moment | null) => {
+    handleChange(prop, value);
   };
 
   const handleTextChange = (prop: FormDataKeys<T>) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newState = { ...formData, [prop]: event.target.value } as Partial<T>;
+    handleChange(prop, event.target.value);
+  };
+
+  const handleChange = (prop: FormDataKeys<T>, value: any) => {
+    const newState = { ...formData, [prop]: value ?? undefined } as Partial<T>;
     setFormData(newState);
     schema
       .validate(newState, { abortEarly: false })
@@ -146,8 +148,9 @@ export default function CustomForm<T extends Record<string, string | number | Da
     createTextField,
     createMoneyField,
     createSubmitButton,
-    createErrorMessages: () => createErrorMessages(submitState.messages),
+    createErrorMessages: () => !submitState.canSubmit && createErrorMessages(submitState.messages),
 
+    handleChange,
     canSubmit: submitState.canSubmit,
     errorMessages: submitState.messages,
     currentValue: formData,
@@ -155,10 +158,7 @@ export default function CustomForm<T extends Record<string, string | number | Da
 
   return (
     <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-      <LocalizationProvider dateAdapter={AdapterLuxon}>
-        {renderFn(renderProps)}
-        {!submitState.canSubmit && createErrorMessages(submitState.messages)}
-      </LocalizationProvider>
+      <LocalizationProvider dateAdapter={AdapterMoment}>{renderFn(renderProps)}</LocalizationProvider>
     </Box>
   );
 }
--- frontend/pages/interest-form.tsx
+++ frontend/pages/interest-form.tsx
@@ -1,124 +1,28 @@
 import { NextPage } from 'next';
-import { useState } from 'react';
 import api from '../frontendApis';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from 'next/link';
-import { useSnackbar } from 'notistack';
-import { useRouter } from 'next/router';
 import { LockOpenOutlined } from '@mui/icons-material';
-import { Alert, AlertTitle, InputAdornment, List, ListItemText } from '@mui/material';
-import { Interest, interestSchema } from '../types/interest';
-import { LocalizationProvider, MobileDatePicker } from '@mui/x-date-pickers';
-import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
+import { Interest, interestSchema, InterestStatus } from '../types/interest';
 import { WithoutId } from '../types/utils';
+import { FormRenderFunctionProps } from '../hooks/useCustomForm';
+import useCustomForm from '../hooks/useCustomForm';
 
 const interestsApi = api.interests;
 const InterestFormPage: NextPage = () => {
-  const { enqueueSnackbar } = useSnackbar();
-  const router = useRouter();
-  const [submitState, setSubmitState] = useState({ canSubmit: false, messages: [] as string[] });
-  const [formData, setFormData] = useState<Partial<WithoutId<Interest>>>();
-
-  const createDatePicker = (field: keyof WithoutId<Interest>, label: string) => (
-    <Grid item xs={12}>
-      <MobileDatePicker
-        inputFormat="EEE, dd MMM yyyy"
-        value={formData ? formData[field] ?? null : null}
-        toolbarTitle={label}
-        onChange={handleDateChange(field)}
-        renderInput={(props) => <TextField {...{ ...props, label }} />}
-      ></MobileDatePicker>
-    </Grid>
-  );
-
-  const createTextField = (field: keyof Interest, label: string) => (
-    <Grid item xs={12}>
-      <TextField
-        required
-        fullWidth
-        name={field}
-        label={label}
-        type="text"
-        id={field}
-        onChange={handleTextChange(field)}
-      />
-    </Grid>
-  );
-
-  const createMoneyField = (field: keyof Interest, label: string) => (
-    <Grid item xs={12}>
-      <TextField
-        required
-        fullWidth
-        name={field}
-        label={label}
-        type="text"
-        id={field}
-        onChange={handleTextChange(field)}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-        }}
-      />
-    </Grid>
-  );
-
-  const createErrorMessages = (messages: string[]) => {
-    return (
-      messages &&
-      messages.length > 0 && (
-        <Alert severity="error">
-          <AlertTitle>Error</AlertTitle>
-          <List>
-            {messages.map((message, index) => (
-              <ListItemText key={index}>{message}</ListItemText>
-            ))}
-          </List>
-        </Alert>
-      )
-    );
-  };
-
-  const handleDateChange = (prop: keyof Interest) => (value: Date | null) => {
-    const newState = { ...formData, [prop]: value ?? undefined };
-    setFormData(newState);
-    interestSchema
-      .validate(newState, { abortEarly: false })
-      .then(() => setSubmitState({ canSubmit: true, messages: [] }))
-      .catch((err) => setSubmitState({ canSubmit: false, messages: err.errors }));
-  };
-
-  const handleTextChange = (prop: keyof Interest) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newState = { ...formData, [prop]: event.target.value };
-    setFormData(newState);
-    interestSchema
-      .validate(newState, { abortEarly: false })
-      .then(() => setSubmitState({ canSubmit: true, messages: [] }))
-      .catch((err) => setSubmitState({ canSubmit: false, messages: err.errors }));
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    interestSchema
-      .validate(formData)
-      .catch((error) => {
-        error.errors.map((m: string) => enqueueSnackbar(m, { variant: 'error' }));
-        throw error;
-      })
-      .then((x) => interestsApi.addInterest(x) /* Interceptor will enqueue snackbar on success / error. */)
-      .then(() => router.push('/admin'))
-      .catch(/* errors from validate and api have already been handled and can be ignored. */);
-  };
+  const interestForm = useCustomForm<WithoutId<Interest>>({
+    // Set charities to [{ id: 1 }] because its not worth building charity selection component rn.
+    // Set status to PENDING as this value cannot be changed by the form.
+    initialData: { charities: [{ id: 1 }], status: InterestStatus.PENDING },
+    schema: interestSchema,
+    renderFn: renderForm,
+    submitFn: (x) => interestsApi.addInterest(x),
+  });
 
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
       <Box
         sx={{
           marginTop: 8,
@@ -130,36 +34,36 @@ const InterestFormPage: NextPage = () => {
         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
           <LockOpenOutlined />
         </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <LocalizationProvider dateAdapter={AdapterLuxon}>
-            <Grid container spacing={2}>
-              {createTextField('donorName', 'Name')}
-              {createTextField('donorEmail', 'Email')}
-              {createTextField('campaignName', 'Campaign name')}
-              {createTextField('campaignDescription', 'Campaign description')}
-              {createTextField('charities', 'Charities')}
-              {createMoneyField('promisedAmount', 'Promised Amount')}
-              {createMoneyField('couponDenomination', 'Coupon Denomination')}
-              {createDatePicker('start', 'Start Date')}
-              {createDatePicker('end', 'End Date')}
-            </Grid>
-            <Button type="submit" disabled={!submitState.canSubmit} fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-              Sign In
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/admin/sign-up">{`Don't have an account? Sign up`}</Link>
-              </Grid>
-            </Grid>
-            {!submitState.canSubmit && createErrorMessages(submitState.messages)}
-          </LocalizationProvider>
-        </Box>
+        {interestForm}
       </Box>
     </Container>
   );
 };
+
+function renderForm({
+  createDatePicker,
+  createErrorMessages,
+  createMoneyField,
+  createSubmitButton,
+  createTextField,
+}: FormRenderFunctionProps<WithoutId<Interest>>) {
+  return (
+    <>
+      <Grid container spacing={2}>
+        {createTextField('donorName', 'Name')}
+        {createTextField('donorEmail', 'Email')}
+        {createTextField('campaignName', 'Campaign name')}
+        {createTextField('campaignDescription', 'Campaign description')}
+        {/* Charity selection is omitted as the model is TBD and its hard to visualize what it will look like. */}
+        {createMoneyField('promisedAmount', 'Promised Amount')}
+        {createMoneyField('couponDenomination', 'Coupon Denomination')}
+        {createDatePicker('start', 'Start Date')}
+        {createDatePicker('end', 'End Date')}
+      </Grid>
+      {createSubmitButton('Submit')}
+      {createErrorMessages()}
+    </>
+  );
+}
 
 export default InterestFormPage;
