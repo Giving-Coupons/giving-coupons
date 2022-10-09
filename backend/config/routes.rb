0c99473a0ee37bd193f0c0286c34f974b05f75be
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-10-09 23:32:50 +0800

    Fix duplicate routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :campaigns
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   scope :api do
     scope :v1 do
