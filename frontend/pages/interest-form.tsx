9d3259ee9c4dac00a2858e991d4367f4b74e5023
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-10-10 01:23:22 +0800

    Use Formik instead.

--- frontend/pages/interest-form.tsx
+++ frontend/pages/interest-form.tsx
@@ -1,50 +1,128 @@
 import { NextPage } from 'next';
+import * as Yup from 'yup';
 import api from '../frontendApis';
 import Avatar from '@mui/material/Avatar';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import { MailOutline } from '@mui/icons-material';
-import { Interest, interestSchema, InterestStatus } from '../types/interest';
+import { Interest, InterestStatus } from '../types/interest';
 import { WithoutId } from '../types/utils';
-import { FormRenderFunctionProps } from '../hooks/useCustomForm';
-import useCustomForm from '../hooks/useCustomForm';
-import { Stack, Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { Button, InputAdornment, Stack, Typography } from '@mui/material';
 import { formStackSx, mailIconSx } from '../styles/interestForm';
+import { useFormik } from 'formik';
+import { useSnackbar } from 'notistack';
+import moment, { Moment } from 'moment';
+import { LocalizationProvider, MobileDatePicker } from '@mui/x-date-pickers';
+import { MuiTextFieldProps } from '@mui/x-date-pickers/internals';
 
+const DEFAULT_COUPON_DENOMINATION = 10;
 const interestsApi = api.interests;
-const InterestFormPage: NextPage = () => {
-  const renderForm = ({
-    createDatePicker,
-    createErrorMessages,
-    createMoneyField,
-    createSubmitButton,
-    createTextField,
-  }: FormRenderFunctionProps<WithoutId<Interest>>) => (
-    <>
-      <Grid container spacing={2}>
-        {createTextField('donorName', 'Name')}
-        {createTextField('donorEmail', 'Email')}
-        {createTextField('campaignName', 'Campaign name')}
-        {createTextField('campaignDescription', 'Campaign description')}
-        {/* TODO: Charity selection is omitted as the model is TBD. */}
-        {createMoneyField('promisedAmount', 'Promised Amount')}
-        {createDatePicker('start', 'Start Date')}
-        {createDatePicker('end', 'End Date')}
-      </Grid>
-      {createSubmitButton('Submit')}
-      {createErrorMessages()}
-    </>
-  );
+type InterestFormData = Omit<Interest, 'id' | 'charities' | 'status' | 'couponDenomination'>;
 
-  const interestForm = useCustomForm<WithoutId<Interest>>({
-    // Set charities to [{ id: 1 }] because its not worth building charity selection component rn.
-    // Set status to PENDING as this value cannot be changed by the form.
-    // Set coupon denomination to $10 as we currently do not plan on allowing people to submit other variations.
-    initialData: { charities: [{ id: 1 }], status: InterestStatus.PENDING, couponDenomination: 10 },
-    schema: interestSchema,
-    renderFn: renderForm,
-    submitFn: (x) => interestsApi.addInterest(x),
+export const interestFormSchema = Yup.object({
+  donorName: Yup.string().required('Donor name is required.'),
+  donorEmail: Yup.string().required('Donor email is required.').email('Donor email is not in the correct form.'),
+  campaignName: Yup.string().required('Campaign name is required.'),
+  campaignDescription: Yup.string().required('Campaign description is required.'),
+  promisedAmount: Yup.number()
+    .required('Promised amount is required.')
+    .typeError('Promised amount must be a number.')
+    .integer('Promised amount must be an integer.')
+    .positive('Promised amount must be a positive.')
+    .min(100, 'Promised amount must be at least $100')
+    .test({
+      test: (promisedAmount) => (promisedAmount ?? 0) % DEFAULT_COUPON_DENOMINATION === 0,
+      message: `Promised amount must be a multiple of $${DEFAULT_COUPON_DENOMINATION}.`,
+    }),
+  start: Yup.date().required('Start date is required.').typeError('Start date must be a date.'),
+  end: Yup.date()
+    .required('End date is required.')
+    .typeError('End date must be a date.')
+    .min(Yup.ref('start'), 'End date must be after start date.'),
+  // TODO: charities are not covered in this PR as the model is TBD.
+  // charities: Yup.array(charitySchema).required(),
+});
+
+const InterestFormPage: NextPage = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const onSubmit = (formState: InterestFormData) =>
+    interestFormSchema
+      .validate(formState)
+      .catch((error) => {
+        error.errors.map((m: string) => enqueueSnackbar(m, { variant: 'error' }));
+        throw error;
+      })
+      .then(fillDefaultValues)
+      .then((x) => interestsApi.addInterest(x) /* Interceptor will enqueue snackbar on success / error. */)
+      .catch(/* errors from validate and api have already been handled and can be ignored. */);
+
+  const formik = useFormik({
+    initialValues: {
+      donorName: '',
+      donorEmail: '',
+      campaignName: '',
+      campaignDescription: '',
+      promisedAmount: 0,
+      start: moment(),
+      end: moment(),
+    },
+    validationSchema: interestFormSchema,
+    onSubmit,
   });
+
+  const fillDefaultValues: (arg: InterestFormData) => WithoutId<Interest> = (formData) => {
+    return {
+      ...formData,
+      // TODO: charities are not covered in this PR as its model is TBD.
+      charities: [{ id: 1 }],
+      status: InterestStatus.PENDING,
+      couponDenomination: DEFAULT_COUPON_DENOMINATION,
+    };
+  };
+
+  const textInputPropHelper = (name: keyof InterestFormData) => {
+    return {
+      id: name,
+      name: name,
+      type: 'text',
+      required: true,
+      fullWidth: true,
+      value: formik.values[name],
+      onChange: formik.handleChange,
+      error: formik.touched[name] && Boolean(formik.errors[name]),
+      helperText: formik.touched[name] && (formik.errors[name] as string),
+      onBlur: formik.handleBlur,
+    };
+  };
+
+  const moneyInputPropHelper = (name: keyof InterestFormData) => {
+    return {
+      ...textInputPropHelper(name),
+      // Strip leading zeroes.
+      InputProps: { startAdornment: <InputAdornment position="start">$</InputAdornment> },
+      value: isNaN(formik.values[name] as number) ? formik.values[name] : Number(formik.values[name]).toString(),
+    };
+  };
+
+  const datePickerPropHelper = (name: keyof InterestFormData, label: string) => {
+    return {
+      inputFormat: 'ddd, D MMM yyyy',
+      value: formik.values[name],
+      renderInput: (props: MuiTextFieldProps) => (
+        <TextField
+          {...props}
+          error={formik.touched[name] && Boolean(formik.errors[name])}
+          helperText={formik.touched[name] && (formik.errors[name] as string)}
+          onBlur={formik.handleBlur}
+          label={label}
+          fullWidth
+          required
+        />
+      ),
+    };
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -52,7 +130,7 @@ const InterestFormPage: NextPage = () => {
         <Avatar sx={mailIconSx}>
           <MailOutline />
         </Avatar>
-        <Stack spacing={1}>
+        <Stack spacing={1} marginBottom={2}>
           <Typography component="h1" align="center" variant="h2">
             Contact Us
           </Typography>
@@ -64,7 +142,46 @@ const InterestFormPage: NextPage = () => {
             soon as possible.
           </Typography>
         </Stack>
-        {interestForm}
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+          <form onSubmit={formik.handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField {...textInputPropHelper('donorName')} label="Name" />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField {...textInputPropHelper('donorEmail')} label="Email" />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField {...textInputPropHelper('campaignName')} label="Campaign name" />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField {...textInputPropHelper('campaignDescription')} label="Campaign description" />
+              </Grid>
+              {/* TODO: Charity selection is omitted as its model is TBD. */}
+              <Grid item xs={12}>
+                <TextField {...moneyInputPropHelper('promisedAmount')} label="Promised Amount" />
+              </Grid>
+              <Grid item xs={12}>
+                <MobileDatePicker
+                  {...datePickerPropHelper('start', 'Start Date')}
+                  disablePast
+                  onChange={(value: Moment | null) => formik.setFieldValue('start', value?.startOf('day'), true)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <MobileDatePicker
+                  {...datePickerPropHelper('end', 'End Date')}
+                  minDate={formik.values.start.clone()}
+                  onChange={(value: Moment | null) => formik.setFieldValue('end', value?.endOf('day'), true)}
+                />
+              </Grid>
+            </Grid>
+            <Button type="submit" disabled={!formik.isValid} fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Submit
+            </Button>
+          </form>
+          <Button onClick={() => console.dir(formik)}>Foo</Button>
+        </LocalizationProvider>
       </Stack>
     </Container>
   );
--- frontend/types/interest.ts
+++ frontend/types/interest.ts
@@ -1,5 +1,4 @@
-import { Charity, charitySchema } from './charity';
-import * as Yup from 'yup';
+import { Charity } from './charity';
 import { Moment } from 'moment';
 
 export enum InterestStatus {
@@ -15,8 +14,8 @@ export type Interest = {
   campaignName: string;
   campaignDescription: string;
   promisedAmount: number;
-  start: Moment;
-  end: Moment;
+  start: Moment | Date;
+  end: Moment | Date;
   status: InterestStatus;
   charities: Charity[];
   couponDenomination: number;
@@ -32,37 +31,3 @@ export type InterestData = Omit<Interest, 'start' | 'end'> & {
   start: string;
   end: string;
 };
-
-export const interestSchema = Yup.object({
-  donorName: Yup.string().required('Donor name is required.'),
-  donorEmail: Yup.string().required('Donor email is required.').email('Donor email is not in the correct form.'),
-  campaignName: Yup.string().required('Campaign name is required.'),
-  campaignDescription: Yup.string().required('Campaign description is required.'),
-  promisedAmount: Yup.number()
-    .required('Promised amount is required.')
-    .typeError('Promised amount could not be parsed.')
-    .integer('Promised amount must be an integer.')
-    .positive('Promised amount must be a positive.')
-    .min(100, 'Promised amount must be at least $100')
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    .when('couponDenomination', (couponDenomination: number, schema: any) => {
-      return couponDenomination
-        ? (schema as Yup.AnySchema).test({
-            test: (promisedAmount) => promisedAmount % couponDenomination === 0,
-            message: `Promised amount must be a multiple of $${couponDenomination}.`,
-          })
-        : schema;
-    }),
-  start: Yup.date().required('Start date is required.').typeError('Start date could not be parsed.'),
-  end: Yup.date()
-    .required('End date is required.')
-    .typeError('End date could not be parsed.')
-    .min(Yup.ref('start'), 'End date must be after start date.'),
-  status: Yup.mixed().required().oneOf(Object.values(InterestStatus), 'Status could not be parsed.'),
-  charities: Yup.array(charitySchema).required(),
-  couponDenomination: Yup.number()
-    .required('Coupon denomination is required.')
-    .typeError('Coupon denomination must be a number.')
-    .integer('Coupon denomination must be an integer.')
-    .positive('Coupon denomination must be positive.'),
-});
