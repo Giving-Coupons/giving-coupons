697d33c8334cb5da7f4191dc9e8825dfa1d966d4
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-10-09 12:35:47 +0800

    Add schema types for interest.

--- frontend/types/interest.ts
+++ frontend/types/interest.ts
@@ -1,4 +1,5 @@
-import { Charity } from './charity';
+import { Charity, charitySchema } from './charity';
+import * as Yup from 'yup';
 
 export enum InterestStatus {
   PENDING = 'pending',
@@ -30,3 +31,30 @@ export type InterestData = Omit<Interest, 'start' | 'end'> & {
   start: string;
   end: string;
 };
+
+export const interestSchema = Yup.object({
+  donorName: Yup.string().required('Donor name is required.'),
+  donorEmail: Yup.string().required('Donor email is required.').email('Donor email is not in the correct form.'),
+  campaignName: Yup.string().required('Campaign name is required.'),
+  campaignDescription: Yup.string().required('Campaign description is required.'),
+  promisedAmount: Yup.number()
+    .required('Promised amount is required.')
+    .integer('Promised amount must be an integer.')
+    .positive('Promised amount must be a positive.')
+    .when('couponDenomination', (couponDenomination: number, schema: any) => {
+      return couponDenomination
+        ? (schema as Yup.AnySchema).test({
+            test: (promisedAmount) => promisedAmount % couponDenomination === 0,
+            message: 'Promised amount must be a multiple of the coupon denomination.',
+          })
+        : schema;
+    }),
+  start: Yup.date().required('Start date is required.'),
+  end: Yup.date().required('End date is required.').min(Yup.ref('start'), 'End date must be after start date.'),
+  status: Yup.mixed().required().oneOf(Object.values(InterestStatus)),
+  charities: Yup.array(charitySchema).required(),
+  couponDenomination: Yup.number()
+    .required('Coupon denomination is required.')
+    .integer('Coupon denomination must be an integer.')
+    .positive('Coupon integer must be positive.'),
+});
