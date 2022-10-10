4561e75e41f4c8c8251b9b9d0423d8f1038c44d9
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-10-10 15:28:46 +0800

    Closer to Figma.

--- frontend/pages/interest.tsx
+++ frontend/pages/interest.tsx
@@ -5,11 +5,11 @@ import Avatar from '@mui/material/Avatar';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import { MailOutline } from '@mui/icons-material';
+import { CheckBox, MailOutline } from '@mui/icons-material';
 import { Interest, InterestStatus } from '../types/interest';
 import { WithoutId } from '../types/utils';
 import TextField from '@mui/material/TextField';
-import { Button, InputAdornment, Stack, Typography } from '@mui/material';
+import { Box, Button, Checkbox, InputAdornment, Stack, Typography } from '@mui/material';
 import { formStackSx, mailIconSx } from '../styles/interestForm';
 import { useFormik } from 'formik';
 import { useSnackbar } from 'notistack';
@@ -19,7 +19,9 @@ import { MuiTextFieldProps } from '@mui/x-date-pickers/internals';
 
 const DEFAULT_COUPON_DENOMINATION = 10;
 const interestsApi = api.interests;
-type InterestFormData = Partial<Omit<Interest, 'id' | 'charities' | 'status' | 'couponDenomination'>>;
+type InterestFormData = Partial<Omit<Interest, 'id' | 'charities' | 'status' | 'couponDenomination' | 'end'>> & {
+  lengthOfCampaign: number;
+};
 
 export const interestFormSchema = Yup.object({
   donorName: Yup.string().required('Donor name is required.'),
@@ -36,11 +38,16 @@ export const interestFormSchema = Yup.object({
       test: (promisedAmount) => (promisedAmount ?? 0) % DEFAULT_COUPON_DENOMINATION === 0,
       message: `Promised amount must be a multiple of $${DEFAULT_COUPON_DENOMINATION}.`,
     }),
-  start: Yup.date().required('Start date is required.').typeError('Start date must be a date.'),
-  end: Yup.date()
-    .required('End date is required.')
-    .typeError('End date must be a date.')
-    .min(Yup.ref('start'), 'End date must be after start date.'),
+  start: Yup.date()
+    .required('Start date is required.')
+    .typeError('Start date must be a date.')
+    .min(moment().endOf('day'), 'Start date cannot be today or in the past.'),
+  lengthOfCampaign: Yup.number()
+    .required('Length of campaign must be a positive integer.')
+    .typeError('Length of campaign must be a number.')
+    .integer('Length of campaign must be an integer.')
+    .positive('Length of campaign must be positive')
+    .max(31, 'Length of campaign cannot be longer than a month'),
   // TODO: charities are not covered in this PR as the model is TBD.
   // charities: Yup.array(charitySchema).required(),
 });
@@ -59,22 +66,24 @@ const InterestFormPage: NextPage = () => {
       .catch(/* errors from validate and api have already been handled and can be ignored. */);
 
   const formik = useFormik({
-    initialValues: {},
+    initialValues: { start: null as unknown } as InterestFormData, // To ensure date picker is unset.
     validationSchema: interestFormSchema,
     onSubmit,
   });
 
   const fillDefaultValues: (arg: Required<InterestFormData>) => WithoutId<Interest> = (formData) => {
+    const { lengthOfCampaign, ...data } = formData;
     return {
-      ...formData,
-      // TODO: charities are not covered in this PR as its model is TBD.
-      charities: [{ id: 1 }],
+      ...data,
       status: InterestStatus.PENDING,
       couponDenomination: DEFAULT_COUPON_DENOMINATION,
+      end: moment(data.start).clone().add(lengthOfCampaign, 'days'),
+      // TODO: charities are not covered in this PR as its model is TBD.
+      charities: [{ id: 1 }],
     };
   };
 
-  const textInputPropHelper = (name: keyof InterestFormData) => {
+  const textInputPropHelper = (name: keyof InterestFormData, label: string, placeholder: string) => {
     return {
       id: name,
       name: name,
@@ -82,32 +91,41 @@ const InterestFormPage: NextPage = () => {
       required: true,
       fullWidth: true,
       value: formik.values[name],
+      onBlur: formik.handleBlur,
       onChange: formik.handleChange,
       error: formik.touched[name] && Boolean(formik.errors[name]),
       helperText: formik.touched[name] && (formik.errors[name] as string),
-      onBlur: formik.handleBlur,
+      // Ensures placeholder is always visible.
+      InputLabelProps: { shrink: true },
+      placeholder,
+      label,
     };
   };
 
-  const moneyInputPropHelper = (name: keyof InterestFormData) => {
+  const integerInputPropHelper = (name: keyof InterestFormData, label: string, placeholder: string) => {
     return {
-      ...textInputPropHelper(name),
+      ...textInputPropHelper(name, label, placeholder),
       // Strip leading zeroes.
-      InputProps: { startAdornment: <InputAdornment position="start">$</InputAdornment> },
       value: isNaN(formik.values[name] as number) ? formik.values[name] : Number(formik.values[name]).toString(),
     };
   };
 
-  const datePickerPropHelper = (name: keyof InterestFormData, label: string) => {
+  const startDateInputPropHelper = (label: string) => {
     return {
       inputFormat: 'ddd, D MMM yyyy',
-      value: formik.values[name],
+      value: formik.values.start,
+      minDate: moment().add(1, 'day').startOf('day'),
+      onChange: async (value: Moment | null) => {
+        const corrected = value === null ? value : value.startOf('day');
+        await formik.setFieldValue('start', corrected, true);
+        await formik.setFieldTouched('start', true);
+      },
+      onClose: () => formik.setFieldTouched('start', true),
       renderInput: (props: MuiTextFieldProps) => (
         <TextField
           {...props}
-          error={formik.touched[name] && Boolean(formik.errors[name])}
-          helperText={formik.touched[name] && (formik.errors[name] as string)}
-          onBlur={formik.handleBlur}
+          error={formik.touched.start && Boolean(formik.errors.start)}
+          helperText={formik.touched.start && (formik.errors.start as string)}
           label={label}
           fullWidth
           required
@@ -117,7 +135,7 @@ const InterestFormPage: NextPage = () => {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="sm">
       <Stack sx={formStackSx}>
         <Avatar sx={mailIconSx}>
           <MailOutline />
@@ -132,36 +150,74 @@ const InterestFormPage: NextPage = () => {
           <Typography variant="body2"></Typography>
         </Stack>
         <LocalizationProvider dateAdapter={AdapterMoment}>
-          <form onSubmit={formik.handleSubmit}>
+          <Container component="form" onSubmit={formik.handleSubmit} maxWidth="lg">
             <Stack spacing={2}>
+              {/* TODO: Charity selection is omitted as its model is TBD. */}
+              <Stack spacing={2}>
+                <Typography component="h2" variant="h3">
+                  Your Campaign
+                </Typography>
+                <TextField {...textInputPropHelper('campaignName', 'Name', 'Give your campaign a name.')} />
+                <TextField
+                  {...textInputPropHelper(
+                    'campaignDescription',
+                    'Description',
+                    'What inspired you to start this campaign?',
+                  )}
+                  multiline
+                />
+                <MobileDatePicker {...startDateInputPropHelper('Start Date')} />
+                {/* TODO: Use duration picker instead. */}
+                <Grid>
+                  <TextField
+                    {...integerInputPropHelper('lengthOfCampaign', 'Length of Campaign', '')}
+                    InputProps={{ endAdornment: <InputAdornment position="end">day(s)</InputAdornment> }}
+                  />
+                </Grid>
+              </Stack>
+              <Stack spacing={2}>
+                <Stack spacing={0}>
+                  <Typography component="h2" variant="h3">
+                    Your contribution
+                  </Typography>
+                  <Typography variant="body1">
+                    How much are you donating? All of your money will be converted to $10 coupons for distribution.
+                  </Typography>
+                </Stack>
+                <TextField
+                  {...integerInputPropHelper('promisedAmount', 'Promised Amount', '')}
+                  InputProps={{ startAdornment: <InputAdornment position="start">$</InputAdornment> }}
+                />
+                <Stack spacing={1}>
+                  <Typography variant="body2">Or choose an amount below</Typography>
+                  <Grid direction="row" spacing={2} wrap="wrap">
+                    {[500, 1000, 2500, 5000].map((value) => (
+                      <Button
+                        key={value}
+                        variant="outlined"
+                        onBlur={formik.handleBlur}
+                        sx={{ margin: 1 }}
+                        onClick={() => formik.handleChange({ target: { name: 'promisedAmount', value } })}
+                      >
+                        ${value}
+                      </Button>
+                    ))}
+                  </Grid>
+                </Stack>
+              </Stack>
               <Stack spacing={2}>
                 <Typography component="h2" variant="h3">
                   Your Details
                 </Typography>
-                <TextField {...textInputPropHelper('donorName')} label="Name" />
-                <TextField {...textInputPropHelper('donorEmail')} label="Email" />
+                <TextField {...textInputPropHelper('donorName', 'Name', '')} />
+                <TextField {...textInputPropHelper('donorEmail', 'Email', '')} />
               </Stack>
-              <TextField {...textInputPropHelper('campaignName')} label="Campaign name" />
-              <TextField {...textInputPropHelper('campaignDescription')} label="Campaign description" />
-              {/* TODO: Charity selection is omitted as its model is TBD. */}
-              <TextField {...moneyInputPropHelper('promisedAmount')} label="Promised Amount" />
-              <MobileDatePicker
-                {...datePickerPropHelper('start', 'Start Date')}
-                disablePast
-                onChange={(value: Moment | null) => formik.setFieldValue('start', value?.startOf('day'), true)}
-              />
-              {/* Use duration picker instead. */}
-              <MobileDatePicker
-                {...datePickerPropHelper('end', 'End Date')}
-                minDate={moment(formik.values.start ?? moment().startOf('day')).clone()}
-                onChange={(value: Moment | null) => formik.setFieldValue('end', value?.endOf('day'), true)}
-              />
             </Stack>
             <Button type="submit" disabled={!formik.isValid} fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
               Submit
             </Button>
-          </form>
-          <Button onClick={() => console.dir(formik)}>Foo</Button>
+            <Button onClick={() => console.dir(formik)}>Fo</Button>
+          </Container>
         </LocalizationProvider>
       </Stack>
     </Container>
