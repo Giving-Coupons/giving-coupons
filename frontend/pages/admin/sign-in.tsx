c02b2c123d77887182ff446c7e70475742f5d218
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-10-10 12:01:17 +0800

    Minor changes

--- frontend/pages/admin/sign-in.tsx
+++ frontend/pages/admin/sign-in.tsx
@@ -1,20 +1,18 @@
-import { NextPage } from 'next';
-import api from '../../frontendApis';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
+import { Stack } from '@mui/material';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import Link from 'next/link';
-import { AdminLoginData, adminLoginDataSchema } from '../../types/admin';
-import { useSnackbar } from 'notistack';
-import { useRouter } from 'next/router';
-import useAdminLoginCheck from '../../hooks/useAdminLogInCheck';
-import { LockOpenOutlined } from '@mui/icons-material';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import { useFormik } from 'formik';
+import { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useSnackbar } from 'notistack';
+import api from '../../frontendApis';
+import useAdminLoginCheck from '../../hooks/useAdminLogInCheck';
+import { AdminLoginData, adminLoginDataSchema } from '../../types/admin';
 
 const adminApi = api.admins;
 
@@ -56,8 +54,11 @@ const SignIn: NextPage = () => {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+    >
       <Box
         sx={{
           marginTop: 8,
@@ -66,10 +67,14 @@ const SignIn: NextPage = () => {
           alignItems: 'center',
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOpenOutlined />
-        </Avatar>
-        <Typography component="h1" variant="h5">
+        <Stack component="div" direction="row" spacing={0.5}>
+          <Typography variant={'h3'}>Giving Coupons</Typography>
+
+          <Typography variant="caption" color="primary">
+            Admin
+          </Typography>
+        </Stack>
+        <Typography component="h1" variant="h5" sx={{ mt: 2 }}>
           Sign in
         </Typography>
         <form onSubmit={formik.handleSubmit}>
@@ -84,7 +89,7 @@ const SignIn: NextPage = () => {
           <Button type="submit" disabled={!formik.isValid} fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
             Sign In
           </Button>
-          <Grid container justifyContent="flex-end">
+          <Grid container justifyContent="center">
             <Grid item>
               <Link href="/admin/sign-up">{`Don't have an account? Sign up`}</Link>
             </Grid>
