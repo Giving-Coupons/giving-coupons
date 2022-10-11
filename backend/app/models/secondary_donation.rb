72eab39b7375e30c5b1e1f3ae8739c8423bb855b
Hong Yao <ganhongyao@u.nus.edu>
2022-10-12 04:30:44 +0800

    Add donation validations

--- backend/app/models/secondary_donation.rb
+++ backend/app/models/secondary_donation.rb
@@ -5,16 +5,26 @@ class SecondaryDonation < ApplicationRecord
   belongs_to :campaign_charity
 
   validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :coupon, final: true
   validate :coupon_must_be_for_campaign_charity
+  validate :coupon_unredeemed, on: :create
 
   private
 
   def coupon_must_be_for_campaign_charity
+    return if coupon.nil?
+
     coupon_campaign = coupon.campaign
-    campaign_charity_campaign = campaign_charity.campaign
+    campaign_charity_campaign = campaign_charity.campaign if campaign_charity
 
     return if coupon_campaign == campaign_charity_campaign
 
     errors.add(:coupon, 'must be for the same campaign as the campaign charity')
+  end
+
+  def coupon_unredeemed
+    return unless coupon.redeemed?
+
+    errors.add(:coupon, 'has already been redeemed')
   end
 end
