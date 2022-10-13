66f22e905c3e52c00b4d39e74313df11313e57e8
Hong Yao <ganhongyao@u.nus.edu>
2022-10-14 00:44:21 +0800

    Implement CouponsController

--- backend/app/controllers/coupons_controller.rb
+++ backend/app/controllers/coupons_controller.rb
@@ -1,15 +1,24 @@
 # frozen_string_literal: true
 
 class CouponsController < ApplicationController
-  before_action :authenticate_admin!
+  # before_action :authenticate_admin!
+  before_action :set_coupon, only: [:show]
 
   def index
-    @coupons = Coupon.all
+    @coupons = Coupon.all.includes({ secondary_donation: { campaign_charity: :charity } })
   end
 
   def unredeemed
-    @coupons = Coupon.all.reject(&:redeemed?)
+    @coupons = Coupon.all.includes(:secondary_donation).where(secondary_donations: { id: nil })
 
-    render :index, status: :ok
+    render :unredeemed, status: :ok
+  end
+
+  def show; end
+
+  private
+
+  def set_coupon
+    @coupon = Coupon.find(params[:id])
   end
 end
