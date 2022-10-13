e8e9fe73525a6aebe05c55e3b5cc9720125e10bc
Geng Ning <gengninghan1@gmail.com>
2022-10-14 01:21:58 +0800

    Improve on button

--- frontend/components/generic/Button.tsx
+++ frontend/components/generic/Button.tsx
@@ -1,5 +1,6 @@
 import { Button as MuiButton, ButtonProps as MuiButtonProps } from '@mui/material';
 import React from 'react';
+import { Map } from 'immutable';
 
 interface Props extends MuiButtonProps {
   actionType: 'primary' | 'secondary' | 'tertiary' | 'muted' | 'danger';
@@ -7,43 +8,37 @@ interface Props extends MuiButtonProps {
   isLabel?: boolean;
 }
 
+const buttonPropsMap = Map<Props['actionType'], Omit<Props, 'actionType'>>({
+  primary: {
+    color: 'primary',
+    variant: 'contained',
+  },
+  secondary: {
+    color: 'primary',
+    variant: 'outlined',
+  },
+  tertiary: {
+    color: 'primary',
+    variant: 'text',
+  },
+  muted: {
+    color: 'neutral',
+    variant: 'outlined',
+  },
+  danger: {
+    color: 'error',
+    variant: 'outlined',
+  },
+});
+
 const Button = ({ actionType, isLabel = false, children, ...rest }: Props) => {
   const component = isLabel ? 'label' : 'button';
 
-  switch (actionType) {
-    case 'primary':
-      return (
-        <MuiButton component={component} color="primary" variant="contained" {...rest}>
-          {children}
-        </MuiButton>
-      );
-    case 'secondary':
-      return (
-        <MuiButton component={component} color="primary" variant="outlined" {...rest}>
-          {children}
-        </MuiButton>
-      );
-    case 'tertiary':
-      return (
-        <MuiButton component={component} color="primary" variant="text" {...rest}>
-          {children}
-        </MuiButton>
-      );
-    case 'muted':
-      return (
-        <MuiButton component={component} color="neutral" variant="outlined" {...rest}>
-          {children}
-        </MuiButton>
-      );
-    case 'danger':
-      return (
-        <MuiButton component={component} color="error" variant="outlined" {...rest}>
-          {children}
-        </MuiButton>
-      );
-    default:
-      return null;
-  }
+  return (
+    <MuiButton component={component} {...buttonPropsMap.get(actionType)} {...rest}>
+      {children}
+    </MuiButton>
+  );
 };
 
 export default Button;
