c8e836be5d64ec824f94e8f47f859df36307bbce
Siva <sivayogasubramanian@gmail.com>
2022-10-14 23:07:09 +0800

    Delete Base64.ts

--- frontend/types/Base64.ts
+++
@@ -1,4 +0,0 @@
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs
-
-// A Base64 string will be in the form of data:[<mediatype>][;base64],<data>
-export type Base64String = string;
