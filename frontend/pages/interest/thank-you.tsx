f4b39aece784a09665f972fcd9204f4752db0dfd
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-10-14 17:41:19 +0800

    Fix PR comments

--- frontend/pages/interest/thank-you.tsx
+++ frontend/pages/interest/thank-you.tsx
@@ -1,26 +1,24 @@
-import { NextPage } from 'next';
+import { Box, Stack, Typography } from '@mui/material';
 import Lottie from 'lottie-react';
+import { NextPage } from 'next';
 import orangeTick from '../../assets/orangeTickLottie.json';
-import { Box, Container, Stack, Typography } from '@mui/material';
-import { stackSx } from '../../styles/interest/thankyou';
+import { lottieSx, stackSx } from '../../styles/interest/thankyou';
 
 const ThankYou: NextPage = () => {
   return (
-    <div>
-      <Container component="main" maxWidth="md">
-        <Stack spacing={2} sx={stackSx}>
-          <Box>
-            <Lottie autoPlay loop={false} animationData={orangeTick} />
-          </Box>
-          <Typography variant="h1">Thank you for getting in touch!</Typography>
-          <Typography variant="body1" textAlign="center">
-            We are excited to find new partners and bring the gift of giving to more people.
-            <br />
-            Give us some time to go over your submission, and we will reach out by email to contact you.
-          </Typography>
-        </Stack>
-      </Container>
-    </div>
+    <Stack component="main" maxWidth="md" spacing={2} sx={stackSx}>
+      <Box sx={lottieSx}>
+        <Lottie autoPlay loop={false} animationData={orangeTick} />
+      </Box>
+      <Typography variant="h1" textAlign="center">
+        Thank you for getting in touch!
+      </Typography>
+      <Typography variant="body1" textAlign="center">
+        We are excited to find new partners and bring the gift of giving to more people.
+        <br />
+        Give us some time to go over your submission, and we will reach out by email to contact you.
+      </Typography>
+    </Stack>
   );
 };
 export default ThankYou;
