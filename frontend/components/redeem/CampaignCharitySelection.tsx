30daa91cd8ae714420962fa406e01bd4b056f6a8
Siva <sivayogasubramanian@gmail.com>
2022-10-16 05:34:05 +0800

    Modify styles for CampaignCharitySelection component

--- frontend/components/redeem/CampaignCharitySelection.tsx
+++ frontend/components/redeem/CampaignCharitySelection.tsx
@@ -18,23 +18,19 @@ type Props = {
 
 const CampaignCharitySelection = ({ coupon, campaignCharityId, setCampaignCharityId, goToNextPage }: Props) => {
   return (
-    <Grid container sx={containerSx} component="main" justifyContent="center" paddingBottom={2}>
-      <Grid item md={12} lg={4} container paddingLeft={2} paddingRight={2} paddingBottom={2}>
-        <Grid xs={12} item paddingLeft={2} paddingRight={2}>
-          <Stack spacing={theme.spacing(2)}>
-            <Stack spacing={theme.spacing(1)}>
-              <Typography variant="h2">{coupon.campaign.name}</Typography>
+    <Grid container sx={containerSx} component="main" justifyContent="center" padding={2}>
+      <Grid item md={12} lg={4}>
+        <Stack spacing={theme.spacing(2)} paddingBottom={4}>
+          <Typography variant="h2">{coupon.campaign.name}</Typography>
 
-              <CampaignDescription campaign={coupon.campaign} />
-            </Stack>
+          <CampaignDescription campaign={coupon.campaign} />
 
-            <CampaignCharityList campaignCharities={coupon.charities} />
-          </Stack>
-        </Grid>
+          <CampaignCharityList campaignCharities={coupon.charities} />
+        </Stack>
       </Grid>
 
-      <Grid item md={12} lg={8} container spacing={2} paddingLeft={2} paddingRight={2} paddingBottom={2}>
-        <Grid xs={12} padding={2}>
+      <Grid item md={12} lg={8} container spacing={4}>
+        <Grid item xs={12}>
           <Typography textAlign="center">You can make a difference too. Choose a coupon recipient!</Typography>
 
           <Typography variant="h3" textAlign="center" color={theme.palette.primary.main}>
@@ -44,33 +40,20 @@ const CampaignCharitySelection = ({ coupon, campaignCharityId, setCampaignCharit
 
         {coupon.charities.map((campaignCharity, index) => (
           <Grid item xs={12} sm={6} key={index}>
-            <Stack
-              direction="row"
-              justifyContent="start"
-              alignItems="flex-start"
-              onClick={() => setCampaignCharityId(campaignCharity.id)}
-            >
+            <Stack direction="row" alignItems="flex-start" onClick={() => setCampaignCharityId(campaignCharity.id)}>
               <Radio checked={campaignCharityId === campaignCharity.id} value={campaignCharity.id} />
-
               <CampaignCharityCard campaignCharity={makeMockCampaignCharity(1)} />
             </Stack>
           </Grid>
         ))}
       </Grid>
 
-      <Grid
-        item
-        xs={12}
-        alignItems="flex-end"
-        justifyContent="flex-end"
-        container
-        paddingLeft={2}
-        paddingRight={2}
-        paddingBottom={2}
-      >
-        <Button actionType="primary" disabled={!campaignCharityId} onClick={goToNextPage}>
-          Next
-        </Button>
+      <Grid item xs={12} paddingTop={2}>
+        <Stack alignItems="flex-end">
+          <Button actionType="primary" disabled={!campaignCharityId} onClick={goToNextPage}>
+            Next
+          </Button>
+        </Stack>
       </Grid>
     </Grid>
   );
