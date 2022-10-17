66775d80ba66dcc763ef7c7d99a85dda33d3b567
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-10-17 14:13:58 +0800

    Clean up pages with Formik component

--- frontend/pages/admin/sign-up.tsx
+++ frontend/pages/admin/sign-up.tsx
@@ -1,20 +1,16 @@
 import { Stack } from '@mui/material';
-import Box from '@mui/material/Box';
 import * as Yup from 'yup';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import { useFormik } from 'formik';
+import { Box, Container, Grid, Typography } from '@mui/material';
+import { Form, Formik } from 'formik';
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useSnackbar } from 'notistack';
 import api from '../../frontendApis';
 import useAdminLoginCheck from '../../hooks/useAdminLogInCheck';
 import { AdminPostData } from '../../types/admin';
 import { boxSx, h1Sx, mainSx, submitButtonSx } from '../../styles/admin/sign-up';
+import FormTextInput from '../../components/forms/FormTextInput';
+import Button from '../../components/generic/Button';
 
 const adminApi = api.admins;
 
@@ -32,42 +28,20 @@ const adminPostDataSchema = Yup.object({
 const SignUp: NextPage = () => {
   useAdminLoginCheck();
   const router = useRouter();
-  const { enqueueSnackbar } = useSnackbar();
 
-  const onSubmit = (formState: AdminPostData) =>
-    adminPostDataSchema
-      .validate(formState)
-      .catch((error) => {
-        error.errors.map((m: string) => enqueueSnackbar(m, { variant: 'error' }));
-        throw error;
-      })
-      .then((x) => adminApi.registerNewAdmin(x) /* Interceptor will enqueue snackbar on success / error. */)
+  const initialValues = {
+    username: '',
+    password: '',
+    passwordConfirmation: '',
+    masterPassword: '',
+  };
+  const onSubmit = async (formState: AdminPostData) =>
+    adminApi
+      // Interceptor will enqueue snackbar on success / error.
+      .registerNewAdmin(formState)
       .then(() => router.push('/admin/sign-in'))
       // Errors from API and schema have already been handled (user was notified). No further action needed.
       .catch(() => undefined);
-
-  const formik = useFormik({
-    initialValues: {
-      username: '',
-      password: '',
-      passwordConfirmation: '',
-      masterPassword: '',
-    },
-    validationSchema: adminPostDataSchema,
-    onSubmit,
-  });
-
-  const propHelper = (name: keyof AdminPostData) => {
-    return {
-      id: name,
-      name: name,
-      value: formik.values[name],
-      onChange: formik.handleChange,
-      error: formik.touched[name] && Boolean(formik.errors[name]),
-      helperText: formik.touched[name] && formik.errors[name],
-      onBlur: formik.handleBlur,
-    };
-  };
 
   return (
     <Container component="main" maxWidth="xs" sx={mainSx}>
@@ -82,36 +56,35 @@ const SignUp: NextPage = () => {
         <Typography component="h1" variant="h4" sx={h1Sx}>
           Sign up
         </Typography>
-        <form onSubmit={formik.handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField required fullWidth label="Username" type="text" {...propHelper('username')} />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField required fullWidth label="Password" type="password" {...propHelper('password')} />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                label="Retype your password"
-                type="password"
-                {...propHelper('passwordConfirmation')}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField required fullWidth label="Master Password" type="password" {...propHelper('masterPassword')} />
-            </Grid>
-          </Grid>
-          <Button type="submit" disabled={!formik.isValid} fullWidth variant="contained" sx={submitButtonSx}>
-            Sign Up
-          </Button>
-          <Grid container justifyContent="center">
-            <Grid item>
-              <Link href="/admin/sign-in">Already have an account? Sign in</Link>
-            </Grid>
-          </Grid>
-        </form>
+
+        <Formik initialValues={initialValues} validationSchema={adminPostDataSchema} onSubmit={onSubmit}>
+          {({ dirty, isValid }) => (
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <FormTextInput name="username" label="Username" />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormTextInput name="password" label="Password" type="password" />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormTextInput name="passwordConfirmation" label="Retype your password" type="password" />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormTextInput name="masterPassword" label="Master password" type="password" />
+                </Grid>
+              </Grid>
+              <Button fullWidth type="submit" disabled={!isValid || !dirty} actionType="primary" sx={submitButtonSx}>
+                Sign Up
+              </Button>
+              <Grid container justifyContent="center">
+                <Grid item>
+                  <Link href="/admin/sign-in">Already have an account? Sign in</Link>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
       </Box>
     </Container>
   );
