a228b8678146fa26ca593eb4d4bfe99a2ae18536
Hong Yao <ganhongyao@u.nus.edu>
2022-10-19 02:37:15 +0800

    Add redeemed column

--- backend/app/models/coupon.rb
+++ backend/app/models/coupon.rb
@@ -9,10 +9,6 @@ class Coupon < ApplicationRecord
   validates :url_token, presence: true, uniqueness: true
   validates :denomination, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
-  def redeemed?
-    SecondaryDonation.exists?(coupon: self)
-  end
-
   def self.generate_unique_url_token
     token = SecureRandom.alphanumeric(NUM_ALPHANUMERIC_CHARS_IN_TOKEN)
     Coupon.exists?(url_token: token) ? generate_unique_url_token : token
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_085648) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_180806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -109,6 +109,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_085648) do
     t.bigint "campaign_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_redeemed", null: false
     t.index ["campaign_id"], name: "index_coupons_on_campaign_id"
     t.index ["url_token"], name: "index_coupons_on_url_token", unique: true
   end
