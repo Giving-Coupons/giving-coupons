69ec2c4bd25438ed0abbf1b819378840aa97ad2a
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-10-20 22:03:06 +0800

    Do sx cleanup.

--- frontend/components/ResponsiveImage.tsx
+++ frontend/components/ResponsiveImage.tsx
@@ -1,46 +1,13 @@
-import React, { useState, useCallback, useEffect } from 'react';
 import { Box } from '@mui/system';
-import { Grid } from '@mui/material';
-
-const useMediaQuery = (width: number) => {
-  const [targetReached, setTargetReached] = useState(false);
-
-  const updateTarget = useCallback((e: MediaQueryListEvent) => {
-    if (e.matches) {
-      setTargetReached(true);
-    } else {
-      setTargetReached(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    const media = window.matchMedia(`(max-width: ${width}px)`);
-    media.addEventListener('change', updateTarget);
-
-    // Check on mount (callback is not called until a change occurs)
-    if (media.matches) {
-      setTargetReached(true);
-    }
-
-    return () => media.removeEventListener('change', updateTarget);
-  }, []);
-
-  return targetReached;
-};
+import useMediaQuery from '../utils/responsive';
 
 const ResponsiveImage = () => {
   const isBreakpoint = useMediaQuery(768);
   return (
     <Box>
       {isBreakpoint ? (
-        <Box>
-          <Grid item lg={12}>
-            <Box
-              sx={{ display: 'block', height: '400px', width: '100vw' }}
-              component="img"
-              src="/charity-image-blob.png"
-            />
-          </Grid>
+        <Box sx={{ display: 'block', width: '100vw' }}>
+          <Box sx={{ display: 'block', width: '100vw' }} component="img" src="/charity-image-blob.png" />
         </Box>
       ) : (
         <Box sx={{ display: 'block', width: '100vw', marginTop: '2rem' }}>
