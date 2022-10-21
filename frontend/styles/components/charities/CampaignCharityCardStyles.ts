e60f106a8c8f4b2458aa89455597986cc5f6796d
Siva <sivayogasubramanian@gmail.com>
2022-10-21 21:45:17 +0800

    Implement small charity card

--- frontend/styles/components/charities/CampaignCharityCardStyles.ts
+++ frontend/styles/components/charities/CampaignCharityCardStyles.ts
@@ -1,33 +1,19 @@
 import { SxProps } from '@mui/system';
 
-export const graphContainerSx: SxProps = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'end',
-  height: '100%',
-  paddingBottom: '8px',
-};
-
 export const graphSx: SxProps = {
-  height: '1em',
+  height: '0.7em',
   width: '100%',
+  '&>div': { border: 'none' },
 };
 
-export const descriptionContainerSx: SxProps = {
-  width: '100%',
-  overflow: 'auto',
+export const charityLogoContainerSx: SxProps = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 };
 
 export const charityLogoSx: SxProps = {
-  maxHeight: '48px',
-  minHeight: '48px',
-  height: '48px',
-};
-
-export const buttonSx: SxProps = {
-  width: '100%',
-};
-
-export const charityTitleSx: SxProps = {
-  alignItems: 'center',
+  maxHeight: '40px',
+  minHeight: '40px',
+  height: '40px',
 };
