6d9eb4cd084fe7a6f809230ce3baaeb1f6ab7768
owenyap <owen.y.yuhan@gmail.com>
2022-10-26 14:02:40 +0800

    shift fontface to globals

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -8,7 +8,6 @@ import NavBar from '../components/navigation/Navbar';
 import { AxiosInterceptor } from '../frontendApis/interceptor';
 import '../styles/globals.scss';
 import { theme } from '../utils/theme';
-import '../styles/fonts.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const PUBLIC_URL = process.env.NEXT_PUBLIC_BASE_CLIENT_URL;
