bf696ab3cf24dfda7cceb383a4ff0397d80f5719
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-10-26 16:22:42 +0800

    Remove commented out code

--- frontend/pages/admin/campaigns/[campaignId]/coupons.tsx
+++ frontend/pages/admin/campaigns/[campaignId]/coupons.tsx
@@ -1,23 +1,18 @@
-import * as React from 'react';
 import { Box } from '@mui/system';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import * as React from 'react';
 import useSWR from 'swr';
-import CouponFrontA from '../../../../components/coupons/CouponFrontA';
-import CouponFrontB from '../../../../components/coupons/CouponFrontB';
 import CouponBackA from '../../../../components/coupons/CouponBackA';
 import CouponBackB from '../../../../components/coupons/CouponBackB';
+import CouponFrontA from '../../../../components/coupons/CouponFrontA';
+import CouponFrontB from '../../../../components/coupons/CouponFrontB';
 import api from '../../../../frontendApis';
 import CouponsAPI from '../../../../frontendApis/coupons';
 import useAdminLoginCheck from '../../../../hooks/useAdminLogInCheck';
 import { CouponBaseData } from '../../../../types/coupons';
 import { Nullable } from '../../../../types/utils';
 import { canBecomeInteger } from '../../../../utils/numbers';
-// import { ToggleButton, ToggleButtonGroup } from '@mui/material';
-
-// import dynamic from 'next/dynamic';
-
-// const GeneratePDF = dynamic(() => import('../../../../components/coupons/GeneratePDF'), { ssr: false });
 
 const CampaignCoupons = () => {
   useAdminLoginCheck();
@@ -35,13 +30,6 @@ const CampaignCoupons = () => {
       <Head>
         <title>Coupons</title>
       </Head>
-      {/* <GeneratePDF html={ref} /> */}
-      {/* <ToggleButtonGroup color="primary" value={alignment} exclusive onChange={handleChange} aria-label="Platform">
-        <ToggleButton value="frontA">Front A</ToggleButton>
-        <ToggleButton value="backA">Back A</ToggleButton>
-        <ToggleButton value="frontB">Front B</ToggleButton>
-        <ToggleButton value="backB">Back B</ToggleButton>
-      </ToggleButtonGroup> */}
 
       <Box component="div" ref={ref}>
         {coupons?.map((coupon, index) => (
