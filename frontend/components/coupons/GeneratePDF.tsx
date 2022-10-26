8514a89a5f866094d5b39fc035460a35268dcf1a
owenyap <owen.y.yuhan@gmail.com>
2022-10-26 13:44:15 +0800

    integrated coupon designs to coupon generation page

---
+++ frontend/components/coupons/GeneratePDF.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { jsPDF } from 'jspdf';
+import { toPng } from 'html-to-image';
+
+interface Props {
+  html?: React.MutableRefObject<HTMLDivElement>;
+}
+
+const GeneratePDF = ({ html }: Props) => {
+  const generateImage = async () => {
+    const image = await toPng(html.current, { quality: 0.95 });
+    const doc = new jsPDF();
+
+    doc.addImage(image, 'JPEG', 5, 22, 200, 160);
+    doc.save();
+  };
+
+  return (
+    <div className="button-container">
+      <button onClick={generateImage}>Get PDF</button>
+    </div>
+  );
+};
+
+export default GeneratePDF;
--- frontend/pages/admin/campaigns/[campaignId]/coupons.tsx
+++ frontend/pages/admin/campaigns/[campaignId]/coupons.tsx
@@ -1,14 +1,23 @@
+import * as React from 'react';
 import { Box } from '@mui/system';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
-import Coupon from '../../../../components/coupons/Coupon';
+import CouponFrontA from '../../../../components/coupons/CouponFrontA';
+import CouponFrontB from '../../../../components/coupons/CouponFrontB';
+import CouponBackA from '../../../../components/coupons/CouponBackA';
+import CouponBackB from '../../../../components/coupons/CouponBackB';
 import api from '../../../../frontendApis';
 import CouponsAPI from '../../../../frontendApis/coupons';
 import useAdminLoginCheck from '../../../../hooks/useAdminLogInCheck';
 import { CouponBaseData } from '../../../../types/coupons';
 import { Nullable } from '../../../../types/utils';
 import { canBecomeInteger } from '../../../../utils/numbers';
+// import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+
+// import dynamic from 'next/dynamic';
+
+// const GeneratePDF = dynamic(() => import('../../../../components/coupons/GeneratePDF'), { ssr: false });
 
 const CampaignCoupons = () => {
   useAdminLoginCheck();
@@ -19,15 +28,45 @@ const CampaignCoupons = () => {
       : null,
   );
 
+  const ref = React.useRef() as React.MutableRefObject<HTMLDivElement>;
+
+
   return (
     <Box>
       <Head>
         <title>Coupons</title>
       </Head>
+      {/* <GeneratePDF html={ref} /> */}
+      {/* <ToggleButtonGroup color="primary" value={alignment} exclusive onChange={handleChange} aria-label="Platform">
+        <ToggleButton value="frontA">Front A</ToggleButton>
+        <ToggleButton value="backA">Back A</ToggleButton>
+        <ToggleButton value="frontB">Front B</ToggleButton>
+        <ToggleButton value="backB">Back B</ToggleButton>
+      </ToggleButtonGroup> */}
 
-      {coupons?.map((coupon, index) => (
-        <Coupon key={index} coupon={coupon} />
-      ))}
+      <Box component="div" ref={ref}>
+        {coupons?.map((coupon, index) => (
+          <CouponFrontA key={index} coupon={coupon} />
+        ))}
+      </Box>
+
+      <Box component="div" ref={ref}>
+        {coupons?.map((coupon, index) => (
+          <CouponBackA key={index} coupon={coupon} />
+        ))}
+      </Box>
+
+      <Box component="div" ref={ref}>
+        {coupons?.map((coupon, index) => (
+          <CouponFrontB key={index} coupon={coupon} />
+        ))}
+      </Box>
+
+      <Box component="div" ref={ref}>
+        {coupons?.map((coupon, index) => (
+          <CouponBackB key={index} coupon={coupon} />
+        ))}
+      </Box>
     </Box>
   );
 };
