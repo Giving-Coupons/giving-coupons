8b922f0d2afdfd1a446143d331d19e5a6cd4e258
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-10-26 16:27:55 +0800

    Minor changes to CouponBackB

--- frontend/components/coupons/CouponBackB.tsx
+++ frontend/components/coupons/CouponBackB.tsx
@@ -1,14 +1,14 @@
+import { Box, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
-import { Typography, Box } from '@mui/material';
+import { QRCodeSVG } from 'qrcode.react';
 import {
   couponContainerSx,
-  instructionsSx,
-  instructionsLogoSx,
   instructionsItemSx,
   instructionsListSx,
+  instructionsLogoSx,
+  instructionsSx,
   qrCodeSx,
 } from '../../styles/components/coupons/couponStyles';
-import { QRCodeSVG } from 'qrcode.react';
 import { CouponBaseData } from '../../types/coupons';
 
 interface Props {
@@ -24,26 +24,30 @@ const CouponBackB = ({ coupon }: Props) => {
       <Stack sx={instructionsSx} component="div">
         <Stack component="div" direction="row" alignItems="center">
           <Box sx={instructionsLogoSx} component="img" src="/logo-icon.png" />
+
           <Typography variant="h2" color="#FF7300">
             Instructions
           </Typography>
         </Stack>
+
         <Box sx={instructionsListSx}>
           <Typography sx={instructionsItemSx}>
             1. You are now the recipient of a coupon kindly sponsored by a donor.
           </Typography>
+
           <Typography sx={instructionsItemSx}>2. Scan the QR code to access the redemption page.</Typography>
+
           <Typography sx={instructionsItemSx}>
             3. Select a charity or a cause which you would like this money to go towards.
           </Typography>
+
           <Typography sx={instructionsItemSx}>4. Make a personal contribution if you can.</Typography>
+
           <Typography sx={instructionsItemSx}>5. Tell your friends about us!</Typography>
+
           <Box marginTop="16px">
             <Typography variant="h3" color="#00ADEF">
-              Giving Coupons is a social impact project
-            </Typography>
-            <Typography variant="h3" color="#00ADEF">
-              part of CS3216
+              Giving Coupons is a social impact project part of CS3216
             </Typography>
           </Box>
         </Box>
