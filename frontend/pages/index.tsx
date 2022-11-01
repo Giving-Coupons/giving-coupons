e8f966bd6b3be34f57fec6b39b374a05779eec5d
Geng Ning <gengninghan1@gmail.com>
2022-11-02 00:27:00 +0800

    Add headline section

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,247 +1,83 @@
-import { Grid, Typography } from '@mui/material';
-import { Box, Stack } from '@mui/system';
+import { Grid, Typography, useMediaQuery } from '@mui/material';
+import { Box, Stack, useTheme } from '@mui/system';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import ButtonCard from '../components/ButtonCard';
-import Button from '../components/generic/Button';
 import {
-  buttonSx,
-  callToActionSectionGridItemSx,
-  callToActionStackSx,
-  charityImageBlobResponsiveSx,
-  charityImageLargeResponsiveSx,
-  givingCouponsNoticeSx,
-  headlineImageSx,
-  headlineSectionBoxResponsiveSx,
-  headlineTextGridSx,
-  headlineTextStackSx,
-  howItWorksHeaderSx,
-  howItWorksImageSx,
-  howItWorksSectionSx,
-  howItWorksStackSx,
-  largeImpactTextSx,
-  logoIconTextSx,
-  logoTextSx,
-  orgDescriptionSx,
-  statsSectionGridItemStackSx,
-  statsSectionGridItemSx,
-  statsSectionGridSx,
-  statsSectionImageSx,
-  statsSectionSx,
+  desktopDescriptionTextSx,
+  desktopHeadlineTextContainerSx,
+  desktopHeadlineTextSx,
+  headlineBackgroundSx,
+  highlightedTextSx,
+  mobileDescriptionTextSx,
+  mobileHeadlineTextContainerSx,
+  mobileHeadlineTextSx,
+  sectionSx,
+  desktopHeadlineScreenImageSx,
+  mobileHeadlineScreenImageSx,
+  mobileHeadlineImageContainerSx,
 } from '../styles/indexStyles';
-import { log } from '../utils/analytics';
-import { ReactNode } from 'react';
+import { combineSxProps } from '../utils/types';
+import Typed from 'react-typed';
+import Button from '../components/generic/Button';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const router = useRouter();
+
+  const headlineTextContainerSx = isMobile ? mobileHeadlineTextContainerSx : desktopHeadlineTextContainerSx;
+  const headlineTextSx = isMobile ? mobileHeadlineTextSx : desktopHeadlineTextSx;
+  const descriptionTextSx = isMobile ? mobileDescriptionTextSx : desktopDescriptionTextSx;
+  const headlineScreenImageSx = isMobile ? mobileHeadlineScreenImageSx : desktopHeadlineScreenImageSx;
+
   return (
     <Box>
       <Head>
         <title>Giving Coupons</title>
       </Head>
 
-      <HeadlineSection />
+      <Stack component="main" alignItems="center">
+        {isMobile ? (
+          <Box sx={headlineBackgroundSx} component="img" src="/landing-page/mobile-header-background.png" />
+        ) : (
+          <Box sx={headlineBackgroundSx} component="img" src="/landing-page/desktop-header-background.png" />
+        )}
 
-      <StatsSection />
+        <Grid sx={sectionSx} container>
+          <Grid sx={headlineTextContainerSx} item xs={12} md={8}>
+            <Typography sx={headlineTextSx}>Giving the</Typography>
 
-      <ResponsiveCharityImage />
+            <Typography sx={combineSxProps(headlineTextSx, highlightedTextSx)}>Gift of Giving</Typography>
 
-      <HowItWorksSection />
+            <Typography sx={descriptionTextSx}>Experience and share the joy of giving by</Typography>
 
-      <CallToActionSection />
+            <Typed
+              style={descriptionTextSx}
+              strings={[
+                'empowering others to give',
+                'learning how your donations helps charities',
+                'directing donations to beneficiaries',
+                'donating to charities',
+              ]}
+              typeSpeed={40}
+              loop
+            />
 
-      <Stack sx={givingCouponsNoticeSx}>
-        <Typography variant="subtitle1">© Giving Coupons 2022</Typography>
+            <br />
+            <br />
+
+            <Button actionType="primary" onClick={() => router.push('/campaigns')}>
+              Explore our campaigns now
+            </Button>
+          </Grid>
+
+          <Grid item xs={12} md={4} sx={isMobile ? mobileHeadlineImageContainerSx : {}}>
+            <Box sx={headlineScreenImageSx} component="img" src="/landing-page/redeem-screen.png" />
+          </Grid>
+        </Grid>
       </Stack>
     </Box>
-  );
-};
-
-const HeadlineSection = () => (
-  // Adds bottom margin if breakpoint < md to account for extra length from text below image.
-  <Grid sx={headlineSectionBoxResponsiveSx} container spacing={2}>
-    <Grid item xs={12} sm={12} md={6} lg={6}>
-      <Box sx={headlineImageSx} component="img" src="/nusstudents.png" />
-    </Grid>
-
-    <Grid item xs={12} sm={12} md={6} lg={6} sx={headlineTextGridSx}>
-      <Stack sx={headlineTextStackSx}>
-        <Typography variant="hero" align="center" textAlign="left" fontWeight="900">
-          Share the gift of giving
-        </Typography>
-
-        <Typography
-          sx={orgDescriptionSx}
-          variant="subtitle1"
-          align="center"
-          fontSize="20px"
-          textAlign="left"
-          fontWeight="500"
-        >
-          Giving Coupons is an initiative to raise awareness and money for charities by empowering more people to
-          donate.
-        </Typography>
-
-        <Button
-          sx={buttonSx}
-          actionType="primary"
-          href="/campaigns"
-          onClick={() => {
-            log('[Home] Click "Explore our campaigns"');
-          }}
-        >
-          Explore Our Campaigns
-        </Button>
-      </Stack>
-    </Grid>
-  </Grid>
-);
-
-const StatsSection = () => {
-  const StatItem = (props: { imgSrc: string; stat: string; subtitle: string }) => (
-    <Grid item xs={12} sm={12} md={4} lg={4} sx={statsSectionGridItemSx}>
-      <Stack sx={statsSectionGridItemStackSx}>
-        <Box sx={statsSectionImageSx} component="img" src={props.imgSrc} />
-
-        <Typography sx={largeImpactTextSx}>{props.stat}</Typography>
-
-        <Typography variant="subtitle1">{props.subtitle}</Typography>
-      </Stack>
-    </Grid>
-  );
-
-  return (
-    <Stack sx={statsSectionSx}>
-      <Typography variant="h1" align="center">
-        To Date
-      </Typography>
-
-      <Box width="80%">
-        <Grid sx={statsSectionGridSx} container spacing={2}>
-          <StatItem imgSrc="/icon-charity.png" stat="$1,600" subtitle="Additional funds raised for charity" />
-
-          <StatItem imgSrc="/icon-voucher.png" stat="160" subtitle="Coupons issued" />
-
-          <StatItem imgSrc="/icon-love.png" stat="8" subtitle="Charities supported" />
-        </Grid>
-      </Box>
-    </Stack>
-  );
-};
-
-const ResponsiveCharityImage = () => (
-  <>
-    {/* Hides if breakpoint >= md */}
-    <Box sx={charityImageBlobResponsiveSx} component="img" src="/charity-image-blob.png" />
-    {/* Shows if breakpoint < md */}
-    <Box sx={charityImageLargeResponsiveSx} component="img" src="/charity-image.png" />
-  </>
-);
-
-const HowItWorksSection = () => {
-  const GridItem = (props: { imgSrc: string; content: ReactNode; alignTo: 'start' | 'end' }) => (
-    <Grid item xs={12}>
-      <Stack sx={howItWorksStackSx} alignItems={props.alignTo === 'start' ? 'flex-start' : 'flex-end'} spacing={2}>
-        <Stack
-          alignItems="center"
-          justifyContent="space-between"
-          direction={props.alignTo === 'start' ? 'row' : 'row-reverse'}
-        >
-          <Box sx={howItWorksImageSx} component="img" src={props.imgSrc} />
-
-          {props.content}
-        </Stack>
-      </Stack>
-    </Grid>
-  );
-
-  const GridItemText = (props: { children: ReactNode }) => (
-    <Typography sx={{ margin: '2rem' }} variant="h3">
-      {props.children}
-    </Typography>
-  );
-
-  return (
-    <Grid sx={howItWorksSectionSx} container spacing={2}>
-      <Grid item xs={12}>
-        <Typography sx={howItWorksHeaderSx} variant="h1">
-          How It Works
-        </Typography>
-      </Grid>
-
-      <GridItem
-        imgSrc="/public-relation.png"
-        content={<GridItemText>Donors start a campaign by committing a sum to one or more charities</GridItemText>}
-        alignTo="start"
-      />
-
-      <GridItem
-        content={
-          <GridItemText>
-            <Box sx={logoIconTextSx} component="img" src="/logo-icon.png" />{' '}
-            <Typography sx={logoTextSx} component="span" color="primary" variant="h4">
-              GIVING COUPONS
-            </Typography>{' '}
-            generates and prints out coupons
-          </GridItemText>
-        }
-        imgSrc="/icon-voucher.png"
-        alignTo="end"
-      />
-
-      <GridItem
-        imgSrc="/icon-charity.png"
-        content={
-          <GridItemText>
-            Coupon recipients choose which charities gets the donation, and can add their own contributions
-          </GridItemText>
-        }
-        alignTo="start"
-      />
-
-      <GridItem
-        imgSrc="/icon-love.png"
-        content={
-          <GridItemText>The selected charities directly receive the funds from the donor or via Giving.SG</GridItemText>
-        }
-        alignTo="end"
-      />
-    </Grid>
-  );
-};
-const CallToActionSection = () => {
-  const router = useRouter();
-
-  return (
-    <Stack sx={callToActionStackSx} spacing={2}>
-      <Typography variant="h1" align="center">
-        Join Our Mission
-      </Typography>
-
-      <Grid container spacing={2} alignItems="center" justifyContent="center" width="100%" maxWidth="1000px">
-        <Grid item xs={12} sm={12} md={6} lg={6} sx={callToActionSectionGridItemSx}>
-          <ButtonCard
-            title="Create a Campaign"
-            content="Commit a sum and generate coupons to spread the gift of giving"
-            onClick={() => {
-              log('[Home] Click "Create a Campaign"');
-              router.push('/interest');
-            }}
-          />
-        </Grid>
-
-        <Grid item xs={12} sm={12} md={6} lg={6} sx={callToActionSectionGridItemSx}>
-          <ButtonCard
-            title="Contribute to a campaign"
-            content="Learn more about existing campaigns and contribute directly"
-            onClick={() => {
-              log('[Home] Click "Contribute to a campaign"');
-              router.push('/campaigns');
-            }}
-          />
-        </Grid>
-      </Grid>
-    </Stack>
   );
 };
 
--- frontend/styles/indexStyles.ts
+++ frontend/styles/indexStyles.ts
@@ -1,197 +1,76 @@
 import { SxProps } from '@mui/system';
 import { theme } from '../utils/theme';
 
-export const examplesContainerSx: SxProps = {
+export const sectionSx: SxProps = {
+  minHeight: `calc(100vh - ${theme.spacing(2)} - ${theme.mixins.toolbar.minHeight}px)`,
+  display: 'flex',
+  alignItems: 'center',
+  maxWidth: '1200px',
+};
+
+export const headlineBackgroundSx: SxProps = {
+  position: 'absolute',
+  top: '0',
+  height: '100vh',
+  width: '100vw',
+  objectFit: 'cover',
+  zIndex: '-1',
+};
+
+export const desktopHeadlineTextContainerSx: SxProps = {
+  padding: 8,
+  height: '100%',
+};
+
+export const mobileHeadlineTextContainerSx: SxProps = {
+  padding: 2,
   display: 'flex',
   flexDirection: 'column',
+  justifyContent: 'center',
   alignItems: 'center',
-};
-
-export const buttonSx: SxProps = {
-  minWidth: '50%',
-  margin: '10px 0px',
-};
-
-export const imageContainerSx: SxProps = {
-  position: 'relative',
-};
-
-export const imageSx: SxProps = {
-  maxWidth: '100%',
-};
-
-export const imageOverlaySx: SxProps = {
-  width: '100%',
   height: '100%',
-  position: 'absolute',
-  backgroundColor: theme.palette.overlayTranslucent.main,
-  zIndex: 10,
 };
 
-export const graphSx: SxProps = {
-  position: 'absolute',
-  bottom: '2em',
-  left: '50%',
-  transform: 'translate(-50%, 0)',
-  width: '90%',
-  height: '2em',
-  zIndex: 20,
+export const desktopHeadlineTextSx: SxProps = {
+  fontSize: 80,
+  fontWeight: 700,
+  lineHeight: 'normal',
 };
 
-export const primaryTranslucentOverlaySx: SxProps = {
+export const mobileHeadlineTextSx: SxProps = {
+  fontSize: 50,
+  fontWeight: 700,
   textAlign: 'center',
-  color: '#FFFFFF',
-  backgroundColor: theme.palette.primaryTranslucent.main,
+  lineHeight: 'normal',
 };
 
-export const secondaryTranslucentOverlaySx: SxProps = {
-  textAlign: 'center',
-  color: '#FFFFFF',
-  backgroundColor: theme.palette.secondaryTranslucent.main,
+export const highlightedTextSx: SxProps = {
+  background: `-webkit-linear-gradient(0deg, ${theme.palette.primary.main} 10%, ${theme.palette.secondary.main} 80%)`,
+  WebkitBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent',
 };
 
-export const contrastText: SxProps = {
-  color: theme.palette.contrast.dark,
+export const desktopDescriptionTextSx = {
+  fontSize: 20,
+  color: theme.palette.grey[800],
 };
 
-export const headlineSectionBoxResponsiveSx: SxProps = {
-  minHeight: '95vh',
-  width: '100%',
-  marginBottom: { xs: '80px', md: '0px' },
-  alignItems: 'center',
-  justifyContent: 'center',
+export const mobileDescriptionTextSx = {
+  fontSize: 16,
+  color: theme.palette.grey[800],
 };
 
-export const headlineImageSx: SxProps = {
-  display: 'block',
-  height: '100%',
-  width: '100%',
+export const desktopHeadlineScreenImageSx: SxProps = {
+  height: '80vh',
+  width: 'auto',
 };
 
-export const headlineTextGridSx: SxProps = {
+export const mobileHeadlineScreenImageSx: SxProps = {
+  width: '80vw',
+  height: 'auto',
+};
+
+export const mobileHeadlineImageContainerSx: SxProps = {
   display: 'flex',
-  alignItems: 'flex-start',
   justifyContent: 'center',
-};
-
-export const headlineTextStackSx: SxProps = {
-  justifyContent: 'center',
-  alignItems: 'flex-start',
-  height: '100%',
-  width: '80%',
-};
-
-export const statsSectionSx: SxProps = {
-  justifyContent: 'center',
-  alignItems: 'center',
-};
-
-export const statsSectionGridSx: SxProps = {
-  marginTop: '2rem',
-  alignItems: 'center',
-  justifyContent: 'center',
-};
-
-export const statsSectionGridItemSx: SxProps = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-};
-
-export const statsSectionGridItemStackSx: SxProps = {
-  justifyContent: 'flex-start',
-  alignItems: 'center',
-};
-
-export const statsSectionImageSx: SxProps = {
-  display: 'block',
-  height: '100px',
-  width: '100px',
-};
-
-export const charityImageBlobResponsiveSx: SxProps = {
-  display: { xs: 'block', md: 'none' },
-  width: '100vw',
-  objectFit: 'contain',
-};
-
-export const charityImageLargeResponsiveSx: SxProps = {
-  display: { xs: 'none', md: 'block' },
-  marginTop: '2rem',
-  width: '100vw',
-  height: '40vh',
-  objectFit: 'cover',
-};
-
-export const howItWorksSectionSx: SxProps = {
-  minHeight: { xs: '100vh', sm: '100vh' },
-  width: '90%',
-  maxWidth: '800px',
-  margin: 'auto',
-  display: 'flex',
-};
-
-export const howItWorksHeaderSx: SxProps = {
-  marginTop: '80px',
-  textAlign: 'center',
-};
-
-export const howItWorksStackSx: SxProps = {
-  marginTop: '10px',
-  justifyContent: 'space-between',
-  width: '100%',
-};
-
-export const howItWorksImageSx: SxProps = {
-  display: 'block',
-  height: '60px',
-  width: '60px',
-};
-
-export const orgDescriptionSx: SxProps = {
-  marginTop: '5px',
-};
-
-export const largeImpactTextSx: SxProps = {
-  fontSize: '3rem',
-  color: theme.palette.primary.main,
-  fontWeight: 'bold',
-};
-
-export const buttonCard: SxProps = {
-  backgroundImage: 'linear-gradient(to right, #FFF428 , #FF3200)',
-  maxWidth: 345,
-};
-
-export const callToActionStackSx: SxProps = {
-  height: '30vh',
-  justifyContent: 'center',
-  alignItems: 'center',
-  paddingTop: '56px',
-};
-
-export const callToActionSectionGridItemSx: SxProps = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-};
-
-export const givingCouponsNoticeSx: SxProps = {
-  width: '100%',
-  height: '30vh',
-  justifyContent: 'flex-end',
-  alignItems: 'flex-start',
-  padding: '10px',
-};
-
-export const logoIconTextSx: SxProps = {
-  maxHeight: '1.6em',
-  minHeight: '1.6em',
-  height: '1.6em',
-  verticalAlign: 'middle',
-  padding: '0 2px',
-};
-
-export const logoTextSx: SxProps = {
-  verticalAlign: 'middle',
 };
