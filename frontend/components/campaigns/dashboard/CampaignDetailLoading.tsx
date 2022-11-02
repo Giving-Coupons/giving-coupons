a6b1420d4addb6ef3fad50b9441b9892d404aecb
Siva <sivayogasubramanian@gmail.com>
2022-11-02 23:39:18 +0800

    Update CampaignDetailLoading

--- frontend/components/campaigns/dashboard/CampaignDetailLoading.tsx
+++ frontend/components/campaigns/dashboard/CampaignDetailLoading.tsx
@@ -11,8 +11,20 @@ const CampaignDetailLoading = () => {
         <Grid item xs={12}>
           <Stack spacing={2}>
             <Grid container columnSpacing={2}>
-              <Grid item xs={12} md={6}>
-                <Skeleton variant="rectangular" height="50vh" />
+              <Grid item xs={12} md={7}>
+                <Stack component="div" spacing={1}>
+                  <Skeleton variant="rectangular" height="5vh" />
+                  <Skeleton variant="rectangular" height="2.5vh" />
+                  <Stack direction={isMobile ? 'column' : 'row'} spacing={2}>
+                    <Skeleton variant="rectangular" width="100%" height="30vh" />
+
+                    <Stack spacing={1} sx={{ width: '100%' }}>
+                      <Skeleton variant="rectangular" width="100%" height="15vh" />
+                      <Skeleton variant="rectangular" width="100%" height="15vh" />
+                    </Stack>
+                  </Stack>
+                  <Skeleton variant="rectangular" height="4vh" />
+                </Stack>
 
                 {!isMobile && (
                   <Grid item xs={12} marginTop={2}>
@@ -25,13 +37,11 @@ const CampaignDetailLoading = () => {
                 )}
               </Grid>
 
-              <Grid item xs={12} md={6}>
-                <Stack component="div" spacing={2}>
+              <Grid item xs={12} md={5}>
+                <Stack component="div" spacing={1}>
                   <Skeleton variant="rectangular" height="5vh" />
                   <Skeleton variant="rectangular" height="2.5vh" />
-                  <Skeleton variant="rectangular" height="15vh" />
-                  <Skeleton variant="rectangular" height="15vh" />
-                  <Skeleton variant="rectangular" height="5vh" />
+                  <Skeleton variant="rectangular" height="50vh" />
                 </Stack>
               </Grid>
             </Grid>
