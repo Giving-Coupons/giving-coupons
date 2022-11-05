4c03ad66feffb48aa816c4f94a761888cd51bd43
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-11-05 21:54:53 +0800

    (Minor) Clean up use of any in analytics.

--- frontend/utils/analytics.ts
+++ frontend/utils/analytics.ts
@@ -1,15 +1,11 @@
 import { getAnalytics, logEvent } from 'firebase/analytics';
 
-// Disable rule to match API of firebase/analytics logEvent
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function log(event: string, params?: { [key: string]: any }) {
+export function log(event: string, params?: { [key: string]: unknown }) {
   const analytics = getAnalytics();
   logEvent(analytics, event, params);
 }
 
-// Disable rule to match API of firebase/analytics logEvent
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function logException(description: string, fatal: boolean, params?: { [key: string]: any }) {
+export function logException(description: string, fatal: boolean, params?: { [key: string]: unknown }) {
   const analytics = getAnalytics();
   logEvent(analytics, 'exception', {
     description,
