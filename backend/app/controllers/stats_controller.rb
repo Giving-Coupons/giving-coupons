331671152eff20fca7bf476fa29a06a08231364e
Siva <sivayogasubramanian@gmail.com>
2022-11-08 15:05:54 +0800

    Fix rubocop errors

--- backend/app/controllers/stats_controller.rb
+++ backend/app/controllers/stats_controller.rb
@@ -7,11 +7,11 @@ class StatsController < ApplicationController
     contribution_amount_from_primary_donor = redemptions.map { |r| r.coupon.denomination }.sum
     contribution_amount_from_secondary_donors = SecondaryDonation.sum(:amount)
     @total_contribution_amount = contribution_amount_from_primary_donor + contribution_amount_from_secondary_donors
-   
+
     @total_redemption_count = redemptions.count
-    @total_charities_supported  = redemptions.map { |r| r.campaign_charity.charity_id }.uniq.count
+    @total_charities_supported = redemptions.map { |r| r.campaign_charity.charity_id }.uniq.count
   end
-  
+
   def steps
     redemptions = Redemption.includes(:coupon).all
 
